3588cbcb1cb627cc83e0f6d9ae55674fe1834ba6
Ethan <ethan.graham@epfl.ch>
2024-03-21 11:39:58 +0100

    Updated gradle build files   - libs.versions.toml in gradle/ for maintaining depedendency versions   - build.gradle.kts: added with dependencies from sample android project

--- sourcecode/build.gradle.kts
+++ sourcecode/build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    /*
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+     */
 }
