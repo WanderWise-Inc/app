fc3a6238d20539c0d31c6678bcdc49a995a9844e
Oscar Duong <notzeoscar@gmail.com>
2024-03-26 19:37:02 +0100

    Add basic skeleton

--- app/src/main/java/com/github/wanderwise_inc/app/model/location/Itinerary.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/model/location/Itinerary.kt
@@ -1,4 +1,9 @@
 package com.github.wanderwise_inc.app.model.location
 
-class Itinerary {
-}
+data class Itinerary(
+    val locations: List<Location>,
+    val creator: Int,
+    val title: String,
+    val description: String?,
+    val visible: Boolean
+)
