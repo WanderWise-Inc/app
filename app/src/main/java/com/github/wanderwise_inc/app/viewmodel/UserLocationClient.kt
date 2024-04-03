ed6d2bf01953ed01dc552a42f4b43b59ff0e8744
Oscar Duong <notzeoscar@gmail.com>
2024-04-03 18:13:21 +0200

    Implement getLocationUpdates function

--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/UserLocationClient.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/UserLocationClient.kt
@@ -1,16 +1,58 @@
 package com.github.wanderwise_inc.app.viewmodel
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
+import android.location.LocationManager
+import android.os.Looper
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
 
 class UserLocationClient(
     private val context: Context,
     private val client: FusedLocationProviderClient
 ): LocationClient {
 
+    @SuppressLint("MissingPermission")
     override fun getLocationUpdates(interval: Long): Flow<Location> {
-        TODO("Not yet implemented")
+        return callbackFlow {
+            if(!context.hasLocationPermission()) {
+                throw LocationClient.LocationException("Location permissions denied")
+            }
+
+            val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            val isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+            if(!isGPSEnabled && !isNetworkEnabled) {
+                throw LocationClient.LocationException("No mean of tracking position")
+            }
+
+            val request = LocationRequest.Builder(interval).build()
+
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(result: LocationResult) {
+                    super.onLocationResult(result)
+                    result.locations.lastOrNull()?.let { location ->
+                        launch { send(location) }
+                    }
+                }
+            }
+
+            client.requestLocationUpdates(
+                request,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+
+            awaitClose {
+                client.removeLocationUpdates(locationCallback)
+            }
+        }
     }
 }
