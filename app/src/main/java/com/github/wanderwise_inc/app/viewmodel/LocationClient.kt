ccee39ba597880ae98b0d2f5cc98cfa7a1906950
Oscar Duong <notzeoscar@gmail.com>
2024-04-03 17:04:33 +0200

    Add comments

--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationClient.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationClient.kt
@@ -4,7 +4,19 @@ import android.location.Location
 import kotlinx.coroutines.flow.Flow
 
 interface LocationClient {
+
+    /**
+     * @brief get the updated location
+     *
+     * @param interval the frequency of the updates in ms
+     * @return a flow containing the updated locations
+     */
     fun getLocationUpdates(interval: Long): Flow<Location>
 
+    /**
+     * @brief exception class for error with the LocationClient
+     *
+     * @param message the description of the exception
+     */
     class LocationException(message: String): Exception()
 }
