b252647d7dc9e2d2ba62e9b7f4b22983376ce659
Ethan <ethan.graham@epfl.ch>
2024-04-11 19:00:28 +0200

    ProfileViewModel: - now uses repository class as data source -> we can mock is easily - added API for getting and setting profiles in the data source - consistent with MapViewModel

--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/ProfileViewModel.kt
@@ -3,9 +3,39 @@ package com.github.wanderwise_inc.app.viewmodel
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModel
+import com.github.wanderwise_inc.app.data.ProfileRepository
+import com.github.wanderwise_inc.app.model.profile.Profile
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.Flow
 
-class ProfileViewModel : ViewModel() {
+class ProfileViewModel(private val profileRepository: ProfileRepository) : ViewModel() {
+    /**
+     * @return flow of a user profile
+     */
+    fun getProfile(userUid: String): Flow<Profile> {
+        return profileRepository.getProfile(userUid)
+    }
+
+    /**
+     * @return all profiles in data source
+     */
+    fun getAllProfiles(): Flow<List<Profile>> {
+        return profileRepository.getAllProfiles()
+    }
+
+    /**
+     * @brief sets a profile
+     */
+    fun setProfile(profile: Profile) {
+        profileRepository.setProfile(profile)
+    }
+
+    /**
+     * @brief deletes a profile from the data source
+     */
+    fun deleteProfile(profile: Profile) {
+        profileRepository.deleteProfile(profile)
+    }
 
     public fun sendResetPassword() {
         val firebaseAuth = FirebaseAuth.getInstance()
