81bdef0caf02d4709f08988bdfb4d77fa3c4bf83
Ethan <ethan.graham@epfl.ch>
2024-04-11 18:24:15 +0200

    added Profile class and ProfileRepository + test data source

--- app/src/main/java/com/github/wanderwise_inc/app/data/ProfileRepository.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/data/ProfileRepository.kt
@@ -1,4 +1,61 @@
 package com.github.wanderwise_inc.app.data
 
+import com.github.wanderwise_inc.app.model.profile.Profile
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
 interface ProfileRepository {
+    /**
+     * @param userUid the uid of a user
+     * @return a flow of a queried profile
+     */
+    fun getProfile(userUid: String): Flow<Profile>
+
+    /**
+     * @return a flow of all profiles
+     */
+    fun getAllProfiles(): Flow<List<Profile>>
+
+    /**
+     * @brief sets a user profile
+     */
+    fun setProfile(profile: Profile)
+
+    /**
+     * @brief deletes a user profile. Use sparingly
+     */
+    fun deleteProfile(profile: Profile)
+}
+
+class ProfileRepositoryTestImpl : ProfileRepository {
+    private var profiles = mutableListOf<Profile>()
+    private var uidCtr = 0 // for getting a new uid
+
+    /**
+     * @throws NoSuchElementException if there are no matches
+     */
+    override fun getProfile(userUid: String): Flow<Profile> {
+        return flow {
+            val filteredProfiles = profiles.filter { it.userUid == userUid }
+            emit( filteredProfiles.first() )
+        }
+    }
+
+    override fun getAllProfiles(): Flow<List<Profile>> {
+        return flow { emit(profiles) }
+    }
+
+    override fun setProfile(profile: Profile) {
+        profiles.remove(profile) // remove profile if it already contained
+        if (profile.uid.isBlank()) {
+            profile.uid = uidCtr.toString()
+            uidCtr++
+        }
+        profiles.add(profile)
+    }
+
+    override fun deleteProfile(profile: Profile) {
+        profiles.remove(profile)
+    }
+
 }
