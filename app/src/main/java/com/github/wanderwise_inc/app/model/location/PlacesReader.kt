599a3ca188756ce340d5892a65858c248f6c15ca
Ethan <ethan.graham@epfl.ch>
2024-04-12 16:58:34 +0200

    added a function for reading locations from a JSON string, and added a sample json file for previewing purposes

--- app/src/main/java/com/github/wanderwise_inc/app/model/location/PlacesReader.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/model/location/PlacesReader.kt
@@ -11,14 +11,14 @@ import java.io.InputStreamReader
 /**
  * Reads a list of place JSON objects from the file places.json.
  */
-class PlacesReader(private val context: Context) {
+class PlacesReader(private val context: Context?) {
 
     // GSON object responsible for converting from JSON to a Place object
     private val gson = Gson()
 
     // InputStream representing places.json
     private val inputStream: InputStream
-        get() = context.resources.openRawResource(R.raw.places)
+        get() = context!!.resources.openRawResource(R.raw.places)
 
     /**
      * Reads the list of place JSON objects in the file places.json and returns a list of Location
@@ -28,6 +28,13 @@ class PlacesReader(private val context: Context) {
         val itemType = object : TypeToken<List<PlaceResponse>>() {}.type
         val reader = InputStreamReader(inputStream)
         return gson.fromJson<List<PlaceResponse>>(reader, itemType).map {
+            it.toLocation()
+        }
+    }
+
+    fun readFromString(): List<Location> {
+        val itemType = object : TypeToken<List<PlaceResponse>>() {}.type
+        return gson.fromJson<List<PlaceResponse>>(SF_LOCATIONS, itemType).map {
             it.toLocation()
         }
     }
