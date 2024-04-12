2e758f4e75811ce7d64ba5211072b115eb26c82c
YofiY <theburnface2013@gmail.com>
2024-04-12 23:55:48 +0200

    app runs now

---
+++ app/src/test/java/com/github/wanderwise_inc/app/ui/signin/LoginScreenKtTest.kt
@@ -0,0 +1,16 @@
+package com.github.wanderwise_inc.app.ui.signin
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class LoginScreenKtTest {
+
+    @Test
+    fun loginScreen() {
+    }
+
+    @Test
+    fun signInButton() {
+    }
+}
