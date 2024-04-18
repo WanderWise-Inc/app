35c49d1389278985a626683d48ce09284acc14cd
Oscar Duong <notzeoscar@gmail.com>
2024-04-18 17:53:51 +0200

    Format

--- app/src/test/java/com/github/wanderwise_inc/app/viewmodel/UserLocationClientTest.kt
+++ app/src/test/java/com/github/wanderwise_inc/app/viewmodel/UserLocationClientTest.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.tasks.Task
+import kotlin.IllegalArgumentException
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
@@ -22,134 +23,117 @@ import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mock
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoRule
-import kotlin.IllegalArgumentException
 
 class UserLocationClientTest {
 
-    @get:Rule
-    val mockitoRule: MockitoRule = MockitoJUnit.rule()
+  @get:Rule val mockitoRule: MockitoRule = MockitoJUnit.rule()
 
-    @Mock
-    private lateinit var context: Context
+  @Mock private lateinit var context: Context
 
-    @Mock
-    private lateinit var locationManager: LocationManager
+  @Mock private lateinit var locationManager: LocationManager
 
-    @Mock
-    private lateinit var client: FusedLocationProviderClient
+  @Mock private lateinit var client: FusedLocationProviderClient
 
-    private lateinit var userLocationClient: UserLocationClient
+  private lateinit var userLocationClient: UserLocationClient
 
-    private val epflLat = 46.519126741544575
-    private val epflLon = 6.5676006970802145
+  private val epflLat = 46.519126741544575
+  private val epflLon = 6.5676006970802145
 
-    @Before
-    fun setup() {
-        userLocationClient = UserLocationClient(context, client)
-    }
+  @Before
+  fun setup() {
+    userLocationClient = UserLocationClient(context, client)
+  }
 
-    @Test
-    fun `getLocationUpdates returns correct location`() = runBlocking {
+  @Test
+  fun `getLocationUpdates returns correct location`() = runBlocking {
+    // Mock necessary dependencies and behavior
+    val location = mock(Location::class.java)
+    location.latitude = epflLat
+    location.longitude = epflLon
+
+    `when`(
+            context.checkPermission(
+                eq(Manifest.permission.ACCESS_COARSE_LOCATION), anyInt(), anyInt()))
+        .thenReturn(PackageManager.PERMISSION_GRANTED)
+    `when`(
+            context.checkPermission(
+                eq(Manifest.permission.ACCESS_FINE_LOCATION), anyInt(), anyInt()))
+        .thenReturn(PackageManager.PERMISSION_GRANTED)
+    `when`(context.getSystemService(Context.LOCATION_SERVICE)).thenReturn(locationManager)
+
+    `when`(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)).thenReturn(true)
+    `when`(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)).thenReturn(true)
+
+    `when`(
+            client.requestLocationUpdates(
+                any(LocationRequest::class.java), any(LocationCallback::class.java), any()))
+        .thenAnswer {
+          val callback = it.arguments[1] as LocationCallback
+          callback.onLocationResult(LocationResult.create(listOf(location)))
+          mock(Task::class.java) as Task<*>
+        }
+
+    // Call the method under test
+    val flow = userLocationClient.getLocationUpdates(1000)
+    val emittedLocation = flow.first()
+
+    // Verify behavior
+    verify(client)
+        .requestLocationUpdates(
+            any(LocationRequest::class.java), any(LocationCallback::class.java), any())
+
+    // Check the emitted location
+    assertEquals(location, emittedLocation)
+  }
+
+  @Test
+  fun `getLocationUpdates throws correct error when interval is invalid`() {
+    assertThrows<IllegalArgumentException> { userLocationClient.getLocationUpdates(0) }
+
+    assertThrows<IllegalArgumentException> { userLocationClient.getLocationUpdates(-1) }
+  }
+
+  @Test
+  fun `getLocationUpdates throws correct error when the permissions are not granted`() =
+      runBlocking<Unit> {
         // Mock necessary dependencies and behavior
-        val location = mock(Location::class.java)
-        location.latitude = epflLat
-        location.longitude = epflLon
-
-        `when`(context.checkPermission(
-            eq(Manifest.permission.ACCESS_COARSE_LOCATION),
-            anyInt(),
-            anyInt()
-        )).thenReturn(PackageManager.PERMISSION_GRANTED)
-        `when`(context.checkPermission(
-            eq(Manifest.permission.ACCESS_FINE_LOCATION),
-            anyInt(),
-            anyInt()
-        )).thenReturn(PackageManager.PERMISSION_GRANTED)
-        `when`(context.getSystemService(Context.LOCATION_SERVICE)).thenReturn(locationManager)
-
-        `when`(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)).thenReturn(true)
-        `when`(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)).thenReturn(true)
-
-        `when`(client.requestLocationUpdates(
-            any(LocationRequest::class.java),
-            any(LocationCallback::class.java),
-            any()
-        )).thenAnswer {
-            val callback = it.arguments[1] as LocationCallback
-            callback.onLocationResult(LocationResult.create(listOf(location)))
-            mock(Task::class.java) as Task<*>
-        }
-
-        // Call the method under test
-        val flow = userLocationClient.getLocationUpdates(1000)
-        val emittedLocation = flow.first()
-
-        // Verify behavior
-        verify(client).requestLocationUpdates(
-            any(LocationRequest::class.java),
-            any(LocationCallback::class.java),
-            any()
-        )
-
-        // Check the emitted location
-        assertEquals(location, emittedLocation)
-    }
-
-    @Test
-    fun `getLocationUpdates throws correct error when interval is invalid`() {
-        assertThrows<IllegalArgumentException> {
-            userLocationClient.getLocationUpdates(0)
-        }
-
-        assertThrows<IllegalArgumentException> {
-            userLocationClient.getLocationUpdates(-1)
-        }
-    }
-
-    @Test
-    fun `getLocationUpdates throws correct error when the permissions are not granted`() = runBlocking<Unit> {
-        // Mock necessary dependencies and behavior
-        `when`(context.checkPermission(
-            eq(Manifest.permission.ACCESS_COARSE_LOCATION),
-            anyInt(),
-            anyInt()
-        )).thenReturn(PackageManager.PERMISSION_DENIED)
-        `when`(context.checkPermission(
-            eq(Manifest.permission.ACCESS_FINE_LOCATION),
-            anyInt(),
-            anyInt()
-        )).thenReturn(PackageManager.PERMISSION_DENIED)
+        `when`(
+                context.checkPermission(
+                    eq(Manifest.permission.ACCESS_COARSE_LOCATION), anyInt(), anyInt()))
+            .thenReturn(PackageManager.PERMISSION_DENIED)
+        `when`(
+                context.checkPermission(
+                    eq(Manifest.permission.ACCESS_FINE_LOCATION), anyInt(), anyInt()))
+            .thenReturn(PackageManager.PERMISSION_DENIED)
 
         val flow = userLocationClient.getLocationUpdates(1000)
 
-        assertThrows<LocationClient.LocationException> {
-            flow.first()
-        }
-    }
+        assertThrows<LocationClient.LocationException> { flow.first() }
+      }
 
-    @Test
-    fun `getLocationUpdates throws correct error when no mean of tracking is available`() = runBlocking<Unit> {
-        `when`(context.checkPermission(
-            eq(Manifest.permission.ACCESS_COARSE_LOCATION),
-            anyInt(),
-            anyInt()
-        )).thenReturn(PackageManager.PERMISSION_GRANTED)
-        `when`(context.checkPermission(
-            eq(Manifest.permission.ACCESS_FINE_LOCATION),
-            anyInt(),
-            anyInt()
-        )).thenReturn(PackageManager.PERMISSION_GRANTED)
+  @Test
+  fun `getLocationUpdates throws correct error when no mean of tracking is available`() =
+      runBlocking<Unit> {
+        `when`(
+                context.checkPermission(
+                    eq(Manifest.permission.ACCESS_COARSE_LOCATION), anyInt(), anyInt()))
+            .thenReturn(PackageManager.PERMISSION_GRANTED)
+        `when`(
+                context.checkPermission(
+                    eq(Manifest.permission.ACCESS_FINE_LOCATION), anyInt(), anyInt()))
+            .thenReturn(PackageManager.PERMISSION_GRANTED)
         `when`(context.getSystemService(Context.LOCATION_SERVICE)).thenReturn(locationManager)
 
         `when`(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)).thenReturn(false)
-        `when`(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)).thenReturn(false)
+        `when`(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+            .thenReturn(false)
 
         assertThrows<LocationClient.LocationException> {
-            userLocationClient.getLocationUpdates(1000).first()
+          userLocationClient.getLocationUpdates(1000).first()
         }
-    }
+      }
 }
