9b20d6c3576aff84520e98bfb8346c482e74352a
Ethan <ethan.graham@epfl.ch>
2024-04-25 17:01:01 +0200

    fixed previewItineraryScreenTest for compatibility

--- app/src/test/java/com/github/wanderwise_inc/app/ui/map/PreviewItineraryScreenKtTest.kt
+++ app/src/test/java/com/github/wanderwise_inc/app/ui/map/PreviewItineraryScreenKtTest.kt
@@ -3,13 +3,14 @@ package com.github.wanderwise_inc.app.ui.map
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.lifecycle.MutableLiveData
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.wanderwise_inc.app.model.location.Itinerary
 import com.github.wanderwise_inc.app.model.location.ItineraryTags
 import com.github.wanderwise_inc.app.model.location.PlacesReader
 import com.github.wanderwise_inc.app.viewmodel.MapViewModel
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.flow.flow
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -46,8 +47,10 @@ class PreviewItineraryScreenKtTest {
     val epflLocation = Mockito.mock(android.location.Location::class.java)
     epflLocation.latitude = epflLat
     epflLocation.longitude = epflLon
-
+    val polylinePoints = MutableLiveData<List<LatLng>>()
+    polylinePoints.value = listOf(LatLng(epflLat, epflLon))
     `when`(mapViewModel.getUserLocation()).thenReturn(flow { emit(epflLocation) })
+    `when`(mapViewModel.getPolylinePointsLiveData()).thenReturn(polylinePoints)
 
     composeTestRule.setContent { PreviewItineraryScreen(itinerary, mapViewModel) }
 
