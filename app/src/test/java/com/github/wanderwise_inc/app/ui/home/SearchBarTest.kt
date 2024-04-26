776c9025c02f7c93fea6f972b7688a7be431df73
Ethan <ethan.graham@epfl.ch>
2024-04-26 09:32:08 +0200

    formatted searchbarTest

--- app/src/test/java/com/github/wanderwise_inc/app/ui/home/SearchBarTest.kt
+++ app/src/test/java/com/github/wanderwise_inc/app/ui/home/SearchBarTest.kt
@@ -1,9 +1,7 @@
 package com.github.wanderwise_inc.app.ui.home
 
 import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createComposeRule
-import org.junit.Rule
-import org.junit.Test
+
 
 /*
 class SearchBarTest {
