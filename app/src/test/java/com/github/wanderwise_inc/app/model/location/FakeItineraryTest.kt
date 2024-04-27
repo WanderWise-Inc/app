de89237930779362061a2b30c458ee268373efcd
Oscar Duong <notzeoscar@gmail.com>
2024-04-27 14:30:32 +0200

    Format

--- app/src/test/java/com/github/wanderwise_inc/app/model/location/FakeItineraryTest.kt
+++ app/src/test/java/com/github/wanderwise_inc/app/model/location/FakeItineraryTest.kt
@@ -1,23 +1,22 @@
 package com.github.wanderwise_inc.app.model.location
 
 import org.junit.Assert.*
-
 import org.junit.Test
 
 class FakeItineraryTest {
 
-    @Test
-    fun getTOKYO() {
-        assertNotNull(FakeItinerary.TOKYO)
-    }
+  @Test
+  fun getTOKYO() {
+    assertNotNull(FakeItinerary.TOKYO)
+  }
 
-    @Test
-    fun getSAN_FRANCISCO() {
-        assertNotNull(FakeItinerary.SAN_FRANCISCO)
-    }
+  @Test
+  fun getSAN_FRANCISCO() {
+    assertNotNull(FakeItinerary.SAN_FRANCISCO)
+  }
 
-    @Test
-    fun getSWITZERLAND() {
-        assertNotNull(FakeItinerary.SWITZERLAND)
-    }
+  @Test
+  fun getSWITZERLAND() {
+    assertNotNull(FakeItinerary.SWITZERLAND)
+  }
 }
