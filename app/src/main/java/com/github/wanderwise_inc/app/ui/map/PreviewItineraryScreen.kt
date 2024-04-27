68e86729d1a7b29cb9bd2aa3cea19ccab0ee6ef0
Ethan <ethan.graham@epfl.ch>
2024-04-27 16:44:33 +0200

    uncommented polyline drawing in previewItineraryScreen

--- app/src/main/java/com/github/wanderwise_inc/app/ui/map/PreviewItineraryScreen.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/ui/map/PreviewItineraryScreen.kt
@@ -30,8 +30,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -78,9 +80,8 @@ fun PreviewItineraryScreen(
     position = CameraPosition.fromLatLngZoom(itinerary.computeCenterOfGravity().toLatLng(), 13f)
   }
 
-  // LaunchedEffect(Unit) { mapViewModel.fetchPolylineLocations(itinerary) }
-  // val polylinePoints by mapViewModel.getPolylinePointsLiveData().observeAsState()
-  val polylinePoints = null
+  LaunchedEffect(Unit) { mapViewModel.fetchPolylineLocations(itinerary) }
+  val polylinePoints by mapViewModel.getPolylinePointsLiveData().observeAsState()
 
   Scaffold(
       bottomBar = { PreviewItineraryBanner(itinerary, mapViewModel, profileViewModel) },
