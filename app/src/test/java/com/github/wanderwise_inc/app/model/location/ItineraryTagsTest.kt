e271b2925c70009980b7b087798d1b786013f7fe
Oscar Duong <notzeoscar@gmail.com>
2024-04-30 21:17:47 +0200

    Add tests

--- app/src/test/java/com/github/wanderwise_inc/app/model/location/ItineraryTagsTest.kt
+++ app/src/test/java/com/github/wanderwise_inc/app/model/location/ItineraryTagsTest.kt
@@ -1,5 +1,27 @@
 package com.github.wanderwise_inc.app.model.location
 
-import org.junit.Assert.*
+import org.junit.Assert.assertNotNull
+import org.junit.Test
 
-class ItineraryTagsTest
+class ItineraryTagsTest {
+
+    @Test
+    fun testItineraryTags() {
+        assertNotNull(ItineraryTags.ACTIVE)
+        assertNotNull(ItineraryTags.ADVENTURE)
+        assertNotNull(ItineraryTags.BUDGET)
+        assertNotNull(ItineraryTags.CULTURAL)
+        assertNotNull(ItineraryTags.FOOD)
+        assertNotNull(ItineraryTags.FOODIE)
+        assertNotNull(ItineraryTags.LUXURY)
+        assertNotNull(ItineraryTags.NATURE)
+        assertNotNull(ItineraryTags.PHOTOGRAPHY)
+        assertNotNull(ItineraryTags.RELAXATION)
+        assertNotNull(ItineraryTags.ROMANCE)
+        assertNotNull(ItineraryTags.RURAL)
+        assertNotNull(ItineraryTags.SOCIAL)
+        assertNotNull(ItineraryTags.URBAN)
+        assertNotNull(ItineraryTags.WELLNESS)
+        assertNotNull(ItineraryTags.WILDLIFE)
+    }
+}
