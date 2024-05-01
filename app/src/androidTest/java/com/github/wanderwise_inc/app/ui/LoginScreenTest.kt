9e87ec29472130f6363da9a9b19383de306f2a57
engjellismaili <engjell.ismaili@epfl.ch>
2024-05-01 17:34:56 +0200

    formatted

--- app/src/androidTest/java/com/github/wanderwise_inc/app/ui/LoginScreenTest.kt
+++ app/src/androidTest/java/com/github/wanderwise_inc/app/ui/LoginScreenTest.kt
@@ -1,23 +1,5 @@
 package com.github.wanderwise_inc.app.ui
 
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import com.github.wanderwise_inc.app.data.GoogleSignInLauncher
-import com.github.wanderwise_inc.app.ui.navigation.graph.Graph
-import com.github.wanderwise_inc.app.ui.signin.LoginScreen
-import io.mockk.MockKAnnotations
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
 /*
 
 @RunWith(MockitoJUnitRunner::class)
