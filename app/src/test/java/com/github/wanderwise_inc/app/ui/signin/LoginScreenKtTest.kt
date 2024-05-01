f5f7986bf07588e682a2c0fd0f491e24ea7c4dd2
engjellismaili <engjell.ismaili@epfl.ch>
2024-05-01 16:03:57 +0200

    Formatted file

--- app/src/test/java/com/github/wanderwise_inc/app/ui/signin/LoginScreenKtTest.kt
+++ app/src/test/java/com/github/wanderwise_inc/app/ui/signin/LoginScreenKtTest.kt
@@ -1,31 +1,22 @@
 package com.github.wanderwise_inc.app.ui.signin
 
-import org.junit.Assert.*
-import org.junit.Test
-
-import android.content.Intent
-import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.navigation.NavHostController
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.github.wanderwise_inc.app.data.GoogleSignInLauncher
-import com.github.wanderwise_inc.app.data.SignInRepositoryImpl
 import com.github.wanderwise_inc.app.ui.navigation.graph.Graph
-import com.github.wanderwise_inc.app.ui.signin.LoginScreen
 import com.github.wanderwise_inc.app.viewmodel.ProfileViewModel
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
@@ -44,7 +35,7 @@ class SignInButtonTest {
     MockKAnnotations.init(this)
     composeTestRule.setContent {
       LoginScreen(
-        googleSignInLauncher, profileViewModel = profileViewModel, navController = navController)
+          googleSignInLauncher, profileViewModel = profileViewModel, navController = navController)
     }
     composeTestRule.waitForIdle()
   }
@@ -53,12 +44,11 @@ class SignInButtonTest {
   fun testLoginScreenWithUserAlreadyPresentInDatabaseShouldNavigate() {
     every { googleSignInLauncher.launchSignIn() } answers { route = Graph.HOME }
 
-
     composeTestRule.onNodeWithText("WanderWise").assertExists()
     composeTestRule.onNodeWithText("WanderWise").assertIsDisplayed()
 
     composeTestRule.onNodeWithText("Sign-In with Google").assertExists()
-    //composeTestRule.onNodeWithText("Sign-In with Google").assertIsDisplayed()
+    // composeTestRule.onNodeWithText("Sign-In with Google").assertIsDisplayed()
 
     composeTestRule.onNodeWithText("Sign-In with Google").performClick()
 
@@ -71,4 +61,3 @@ class MockGoogleSignInLauncher : GoogleSignInLauncher {
     TODO("Not yet implemented")
   }
 }
-
