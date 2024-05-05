6667434df474787d713b0f458d4507ef8fdd9399
Oscar Duong <notzeoscar@gmail.com>
2024-05-05 20:31:17 +0200

    Clean code

--- app/src/main/java/com/github/wanderwise_inc/app/MainActivity.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.github.wanderwise_inc.app
 
-// import com.github.wanderwise_inc.app.data.ProfileRepositoryImpl
 import android.Manifest
 import android.os.Bundle
 import android.util.Log
@@ -17,110 +16,126 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.github.wanderwise_inc.app.data.DefaultGoogleSignInLauncher
-import com.github.wanderwise_inc.app.data.DirectionsRepository
-import com.github.wanderwise_inc.app.data.GoogleSignInLauncher
-import com.github.wanderwise_inc.app.data.ImageRepositoryImpl
-import com.github.wanderwise_inc.app.data.ItineraryRepositoryTestImpl
-import com.github.wanderwise_inc.app.data.ProfileRepositoryTestImpl
-import com.github.wanderwise_inc.app.data.SignInRepositoryImpl
+import com.github.wanderwise_inc.app.data.*
 import com.github.wanderwise_inc.app.network.ApiServiceFactory
 import com.github.wanderwise_inc.app.ui.navigation.graph.RootNavigationGraph
 import com.github.wanderwise_inc.app.ui.theme.WanderWiseTheme
-import com.github.wanderwise_inc.app.viewmodel.BottomNavigationViewModel
-import com.github.wanderwise_inc.app.viewmodel.MapViewModel
-import com.github.wanderwise_inc.app.viewmodel.ProfileViewModel
-import com.github.wanderwise_inc.app.viewmodel.UserLocationClient
+import com.github.wanderwise_inc.app.viewmodel.*
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-  private lateinit var imageRepository: ImageRepositoryImpl
-  private val directionsApiService = ApiServiceFactory.createDirectionsApiService()
-  private val directionsRepository = DirectionsRepository(directionsApiService)
-  private lateinit var mapViewModel: MapViewModel
-  private val signInRepositoryImpl = SignInRepositoryImpl()
-  private lateinit var googleSignInLauncher: GoogleSignInLauncher
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseStorage: FirebaseStorage
 
-  // declaration for use of storage
-  private val storage = FirebaseStorage.getInstance()
-  private var imageReference = storage.reference
-  private lateinit var navController: NavHostController
+    private lateinit var itineraryRepository: ItineraryRepository
+    private lateinit var profileRepository: ProfileRepository
+    private lateinit var imageRepository: ImageRepository
+    private lateinit var directionsRepository: DirectionsRepository
+    private lateinit var signInRepository: SignInRepository
 
-  private lateinit var profileViewModel: ProfileViewModel
-  private lateinit var bottomNavigationViewModel: BottomNavigationViewModel
+    private lateinit var mapViewModel: MapViewModel
+    private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var bottomNavigationViewModel: BottomNavigationViewModel
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    private lateinit var googleSignInLauncher: GoogleSignInLauncher
 
-    val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
-    val signInIntent =
-        AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
+    private lateinit var navController: NavHostController
 
-    val signInLauncher =
-        registerForActivityResult(
-            FirebaseAuthUIActivityResultContract(),
-        ) { res ->
-          lifecycleScope.launch {
-            val user = FirebaseAuth.getInstance().currentUser
-            signInRepositoryImpl.signIn(res, navController, profileViewModel, user, res.resultCode)
-          }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseStorage = FirebaseStorage.getInstance()
+        requestPermissions()
+        initializeRepositories()
+        setupSignIn()
+        initializeViewModels()
+
+        setContent {
+            WanderWiseTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    navController = rememberNavController()
+                    RootNavigationGraph(
+                        googleSignInLauncher,
+                        profileViewModel,
+                        mapViewModel,
+                        bottomNavigationViewModel,
+                        imageRepository,
+                        navController,
+                        firebaseAuth
+                    )
+                }
+            }
         }
-
-    googleSignInLauncher = DefaultGoogleSignInLauncher(signInLauncher, signInIntent)
-
-    val profileRepository = ProfileRepositoryTestImpl()
-    imageRepository = ImageRepositoryImpl(imageLauncher, imageReference, null)
-
-    // Ask for location permissions
-    ActivityCompat.requestPermissions(
-        this,
-        arrayOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
-        0)
-
-    val itineraryRepository = ItineraryRepositoryTestImpl()
-    val userLocationClient =
-        UserLocationClient(
-            applicationContext, LocationServices.getFusedLocationProviderClient(applicationContext))
-
-    mapViewModel = MapViewModel(itineraryRepository, directionsRepository, userLocationClient)
-    profileViewModel = ProfileViewModel(profileRepository, imageRepository)
-    bottomNavigationViewModel = BottomNavigationViewModel()
-
-    setContent {
-      WanderWiseTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          navController = rememberNavController()
-          RootNavigationGraph(
-              googleSignInLauncher = googleSignInLauncher,
-              profileViewModel = profileViewModel,
-              mapViewModel = mapViewModel,
-              bottomNavigationViewModel = bottomNavigationViewModel,
-              imageRepository = imageRepository,
-              navController = navController,
-              firebaseAuth = FirebaseAuth.getInstance(),
-          )
-        }
-      }
     }
-  }
 
-  /**
-   * image launcher Used to set launch an activity that will set the currentFile of the
-   * imageRepository to the selected file by the user (the one in the photo gallery) Launcher that
-   * will be called in the imageRepository
-   */
-  private val imageLauncher =
-      registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-          result?.data?.data.let {
-            imageRepository.setCurrentFile(it)
-            Log.d("STORE IMAGE", "CURRENTFILE SELECTED")
-          }
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            0
+        )
+    }
+
+    private fun initializeRepositories() {
+        val imageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { res ->
+            if (res.resultCode == RESULT_OK) {
+                res.data?.data?.let {
+                    imageRepository.setCurrentFile(it)
+                    Log.d("STORE IMAGE", "CURRENT FILE SELECTED")
+                }
+            }
         }
-      }
+
+        itineraryRepository = ItineraryRepositoryTestImpl()
+        directionsRepository = DirectionsRepository(ApiServiceFactory.createDirectionsApiService())
+        profileRepository = ProfileRepositoryTestImpl()
+        imageRepository = ImageRepositoryImpl(imageLauncher, firebaseStorage.reference, null)
+        signInRepository = SignInRepositoryImpl()
+    }
+
+    private fun setupSignIn() {
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+        val signInIntent = AuthUI
+            .getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+
+        val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
+                lifecycleScope.launch {
+                    val user = FirebaseAuth.getInstance().currentUser
+                    signInRepository.signIn(
+                        res,
+                        navController,
+                        profileViewModel,
+                        user,
+                        res.resultCode
+                    )
+                }
+            }
+
+        googleSignInLauncher = DefaultGoogleSignInLauncher(signInLauncher, signInIntent)
+    }
+
+    private fun initializeViewModels() {
+        val userLocationClient = UserLocationClient(
+            applicationContext,
+            LocationServices.getFusedLocationProviderClient(applicationContext)
+        )
+
+        mapViewModel = MapViewModel(itineraryRepository, directionsRepository, userLocationClient)
+        profileViewModel = ProfileViewModel(profileRepository, imageRepository)
+        bottomNavigationViewModel = BottomNavigationViewModel()
+    }
 }
