da9982a3351d86ce29ce9100995d34b772ebb560
Oscar Duong <notzeoscar@gmail.com>
2024-05-06 14:13:36 +0200

    Clean code

--- app/src/main/java/com/github/wanderwise_inc/app/data/SignInRepository.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/data/SignInRepository.kt
@@ -1,9 +1,6 @@
 package com.github.wanderwise_inc.app.data
 
-import android.app.Activity
-import android.util.Log
 import androidx.navigation.NavController
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.github.wanderwise_inc.app.model.profile.Profile
 import com.github.wanderwise_inc.app.ui.navigation.graph.Graph
 import com.github.wanderwise_inc.app.viewmodel.ProfileViewModel
@@ -11,58 +8,48 @@ import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.flow.first
 
 interface SignInRepository {
-  suspend fun signIn(
-      result: FirebaseAuthUIAuthenticationResult,
-      navController: NavController,
-      profileViewModel: ProfileViewModel,
-      user: FirebaseUser?,
-      resultCode: Int
-  )
+    suspend fun signIn(
+        navController: NavController,
+        profileViewModel: ProfileViewModel,
+        user: FirebaseUser?,
+    )
 }
 
 class SignInRepositoryImpl : SignInRepository {
-  override suspend fun signIn(
-      result: FirebaseAuthUIAuthenticationResult,
-      navController: NavController,
-      profileViewModel: ProfileViewModel,
-      user: FirebaseUser?,
-      resultCode: Int
-  ) {
-    if (user == null) {
-      throw Exception("User is Null")
-    } else {
-      if (resultCode == Activity.RESULT_OK) {
-        Log.d("TESTING SIGN IN BUTTON", "RESULT_OK")
-        // Get the user from database (function that returns a flow)
-        val currentProfile = profileViewModel.getProfile(user.uid).first()
-        Log.d("TESTING SIGN IN BUTTON", "GET PROFILE PASSED")
-
-        // User was already present in the database, in which case we only
-        // navigate to the Home page
-        if (currentProfile != null) {
-          navController.navigate(Graph.HOME)
+    override suspend fun signIn(
+        navController: NavController,
+        profileViewModel: ProfileViewModel,
+        user: FirebaseUser?,
+    ) {
+        if (user == null) {
+            throw Exception("User is Null")
         } else {
-          // We define properly the fields, because some of them could be empty
-          val username = user.displayName
-          val properUsername = username ?: ""
-          val uid = user.uid
-          val description = ""
+            // Get the user from database (function that returns a flow)
+            val currentProfile = profileViewModel.getProfile(user.uid).first()
 
-          val newProfile =
-              Profile(
-                  displayName = properUsername,
-                  userUid = uid,
-                  bio = description,
-                  profilePicture = user.photoUrl)
+            // User was already present in the database, in which case we only
+            // navigate to the Home page
+            if (currentProfile != null) {
+                navController.navigate(Graph.HOME)
+            } else {
+                // We define properly the fields, because some of them could be empty
+                val username = user.displayName
+                val properUsername = username ?: ""
+                val uid = user.uid
+                val description = ""
 
-          // We set the user to the database
-          profileViewModel.setProfile(newProfile)
-          navController.navigate(Graph.HOME)
+                val newProfile =
+                    Profile(
+                        displayName = properUsername,
+                        userUid = uid,
+                        bio = description,
+                        profilePicture = user.photoUrl
+                    )
+
+                // We set the user to the database
+                profileViewModel.setProfile(newProfile)
+                navController.navigate(Graph.HOME)
+            }
         }
-      } else {
-        // unsuccessful sign in
-        throw Exception("User unsuccessful sign in")
-      }
     }
-  }
 }
