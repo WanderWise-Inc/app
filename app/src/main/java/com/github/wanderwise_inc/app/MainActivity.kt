cddbc5d46b7c6fcd971bf85e7b6ae9663dd6f465
Oscar Duong <notzeoscar@gmail.com>
2024-05-06 11:00:30 +0200

    Use AppModule to initialize dependencies (not working)

--- app/src/main/java/com/github/wanderwise_inc/app/MainActivity.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/MainActivity.kt
@@ -2,10 +2,8 @@ package com.github.wanderwise_inc.app
 
 import android.Manifest
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,31 +12,34 @@ import androidx.core.app.ActivityCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.github.wanderwise_inc.app.data.*
-import com.github.wanderwise_inc.app.network.ApiServiceFactory
+import com.github.wanderwise_inc.app.data.DirectionsRepository
+import com.github.wanderwise_inc.app.data.GoogleSignInLauncher
+import com.github.wanderwise_inc.app.data.ImageRepository
+import com.github.wanderwise_inc.app.data.ItineraryRepository
+import com.github.wanderwise_inc.app.data.ProfileRepository
+import com.github.wanderwise_inc.app.data.SignInRepository
+import com.github.wanderwise_inc.app.di.AppModule
 import com.github.wanderwise_inc.app.ui.navigation.graph.RootNavigationGraph
 import com.github.wanderwise_inc.app.ui.theme.WanderWiseTheme
-import com.github.wanderwise_inc.app.viewmodel.*
-import com.google.android.gms.location.LocationServices
+import com.github.wanderwise_inc.app.viewmodel.BottomNavigationViewModel
+import com.github.wanderwise_inc.app.viewmodel.MapViewModel
+import com.github.wanderwise_inc.app.viewmodel.ProfileViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firebaseStorage: FirebaseStorage
 
-    private lateinit var itineraryRepository: ItineraryRepository
-    private lateinit var profileRepository: ProfileRepository
     private lateinit var imageRepository: ImageRepository
+    private lateinit var itineraryRepository: ItineraryRepository
     private lateinit var directionsRepository: DirectionsRepository
+    private lateinit var profileRepository: ProfileRepository
     private lateinit var signInRepository: SignInRepository
 
+    private lateinit var bottomNavigationViewModel: BottomNavigationViewModel
     private lateinit var mapViewModel: MapViewModel
     private lateinit var profileViewModel: ProfileViewModel
-    private lateinit var bottomNavigationViewModel: BottomNavigationViewModel
 
     private lateinit var googleSignInLauncher: GoogleSignInLauncher
 
@@ -71,13 +72,23 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun init() {
-        firebaseAuth = FirebaseAuth.getInstance()
-        firebaseStorage = FirebaseStorage.getInstance()
-
         requestPermissions()
+
+        firebaseAuth = AppModule.provideFirebaseAuth()
+        firebaseStorage = AppModule.provideFirebaseStorage()
+
         initializeRepositories()
-        setupSignIn()
+
         initializeViewModels()
+
+        googleSignInLauncher = AppModule.provideGoogleSignInLauncher(
+            this,
+            lifecycleScope,
+            firebaseAuth,
+            signInRepository,
+            navController,
+            profileViewModel
+        )
     }
 
     private fun requestPermissions() {
@@ -92,55 +103,16 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun initializeRepositories() {
-        val imageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { res ->
-            if (res.resultCode == RESULT_OK) {
-                res.data?.data?.let {
-                    imageRepository.setCurrentFile(it)
-                    Log.d("STORE IMAGE", "CURRENT FILE SELECTED")
-                }
-            }
-        }
-
-        itineraryRepository = ItineraryRepositoryTestImpl()
-        directionsRepository = DirectionsRepository(ApiServiceFactory.createDirectionsApiService())
-        profileRepository = ProfileRepositoryTestImpl()
-        imageRepository = ImageRepositoryImpl(imageLauncher, firebaseStorage.reference, null)
-        signInRepository = SignInRepositoryImpl()
-    }
-
-    private fun setupSignIn() {
-        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
-
-        val signInIntent = AuthUI
-            .getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-
-        val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
-                lifecycleScope.launch {
-                    val user = FirebaseAuth.getInstance().currentUser
-                    signInRepository.signIn(
-                        res,
-                        navController,
-                        profileViewModel,
-                        user,
-                        res.resultCode
-                    )
-                }
-            }
-
-        googleSignInLauncher = DefaultGoogleSignInLauncher(signInLauncher, signInIntent)
+        imageRepository = AppModule.provideImageRepository(this, imageRepository, firebaseStorage)
+        itineraryRepository = AppModule.provideItineraryRepository()
+        directionsRepository = AppModule.provideDirectionsRepository()
+        profileRepository = AppModule.provideProfileRepository()
+        signInRepository = AppModule.provideSignInRepository()
     }
 
     private fun initializeViewModels() {
-        val userLocationClient = UserLocationClient(
-            applicationContext,
-            LocationServices.getFusedLocationProviderClient(applicationContext)
-        )
-
-        mapViewModel = MapViewModel(itineraryRepository, directionsRepository, userLocationClient)
-        profileViewModel = ProfileViewModel(profileRepository, imageRepository)
-        bottomNavigationViewModel = BottomNavigationViewModel()
+        bottomNavigationViewModel = AppModule.provideBottomNavigationViewModel()
+        mapViewModel = AppModule.provideMapViewModel(applicationContext, itineraryRepository, directionsRepository)
+        profileViewModel = AppModule.provideProfileViewModel(profileRepository, imageRepository)
     }
 }
--- app/src/main/java/com/github/wanderwise_inc/app/di/AppModule.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/di/AppModule.kt
@@ -1,5 +1,144 @@
 package com.github.wanderwise_inc.app.di
 
-object AppModule {
+import android.content.Context
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.navigation.NavHostController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.github.wanderwise_inc.app.data.DefaultGoogleSignInLauncher
+import com.github.wanderwise_inc.app.data.DirectionsRepository
+import com.github.wanderwise_inc.app.data.ImageRepository
+import com.github.wanderwise_inc.app.data.ImageRepositoryImpl
+import com.github.wanderwise_inc.app.data.ItineraryRepository
+import com.github.wanderwise_inc.app.data.ItineraryRepositoryTestImpl
+import com.github.wanderwise_inc.app.data.ProfileRepository
+import com.github.wanderwise_inc.app.data.ProfileRepositoryTestImpl
+import com.github.wanderwise_inc.app.data.SignInRepository
+import com.github.wanderwise_inc.app.data.SignInRepositoryImpl
+import com.github.wanderwise_inc.app.network.ApiServiceFactory
+import com.github.wanderwise_inc.app.viewmodel.BottomNavigationViewModel
+import com.github.wanderwise_inc.app.viewmodel.MapViewModel
+import com.github.wanderwise_inc.app.viewmodel.ProfileViewModel
+import com.github.wanderwise_inc.app.viewmodel.UserLocationClient
+import com.google.android.gms.location.LocationServices
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.launch
 
+object AppModule  {
+
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    fun provideFirebaseStorage() = FirebaseStorage.getInstance()
+
+    fun provideImageRepository(
+        activity: ComponentActivity,
+        imageRepository: ImageRepository,
+        firebaseStorage: FirebaseStorage
+    ): ImageRepository {
+        return ImageRepositoryImpl(
+            provideImageLauncher(activity, imageRepository),
+            firebaseStorage.reference,
+            null
+        )
+    }
+
+    fun provideDirectionsRepository() = DirectionsRepository(
+        ApiServiceFactory.createDirectionsApiService()
+    )
+
+    fun provideItineraryRepository() = ItineraryRepositoryTestImpl()
+
+    fun provideProfileRepository() = ProfileRepositoryTestImpl()
+
+    fun provideSignInRepository() = SignInRepositoryImpl()
+
+    fun provideBottomNavigationViewModel() = BottomNavigationViewModel()
+
+    fun provideMapViewModel(
+        context: Context,
+        itineraryRepository: ItineraryRepository,
+        directionsRepository: DirectionsRepository
+    ) = MapViewModel(
+        itineraryRepository,
+        directionsRepository,
+        provideLocationClient(context)
+    )
+
+    fun provideProfileViewModel(
+        profileRepository: ProfileRepository,
+        imageRepository: ImageRepository
+    ) = ProfileViewModel(
+        profileRepository,
+        imageRepository
+    )
+
+    fun provideGoogleSignInLauncher(
+        activity: ComponentActivity,
+        lifecycleScope: LifecycleCoroutineScope,
+        firebaseAuth: FirebaseAuth,
+        signInRepository: SignInRepository,
+        navController: NavHostController,
+        profileViewModel: ProfileViewModel
+    ) = DefaultGoogleSignInLauncher(
+        provideSignInLauncher(
+            activity,
+            lifecycleScope,
+            firebaseAuth,
+            signInRepository,
+            navController,
+            profileViewModel
+        ),
+        provideSignInIntent()
+    )
+
+    private fun provideImageLauncher(
+        activity: ComponentActivity,
+        imageRepository: ImageRepository
+    ) = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { res ->
+        if (res.resultCode == ComponentActivity.RESULT_OK) {
+            res.data?.data?.let {
+                imageRepository.setCurrentFile(it)
+                Log.d("STORE IMAGE", "CURRENT FILE SELECTED")
+            }
+        }
+    }
+
+    private fun provideLocationClient(context: Context) = UserLocationClient(
+        context,
+        LocationServices.getFusedLocationProviderClient(context)
+    )
+
+    private fun provideSignInLauncher(
+        activity: ComponentActivity,
+        lifecycleScope: LifecycleCoroutineScope,
+        firebaseAuth: FirebaseAuth,
+        signInRepository: SignInRepository,
+        navController: NavHostController,
+        profileViewModel: ProfileViewModel
+    ) = activity.registerForActivityResult(FirebaseAuthUIActivityResultContract()) { res ->
+        lifecycleScope.launch {
+            val user = firebaseAuth.currentUser
+            signInRepository.signIn(
+                res,
+                navController,
+                profileViewModel,
+                user,
+                res.resultCode
+            )
+        }
+    }
+
+    private val providers = listOf(
+        AuthUI.IdpConfig.GoogleBuilder().build()
+    )
+
+    private fun provideSignInIntent() = AuthUI
+        .getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .build()
 }
