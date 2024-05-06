d3a7f2c4f20fd68107bb064b6b12ec58c0001a4b
engjellismaili <engjell.ismaili@epfl.ch>
2024-05-06 15:43:34 +0200

    Removed comments

--- app/src/main/java/com/github/wanderwise_inc/app/data/ItineraryRepository.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/data/ItineraryRepository.kt
@@ -37,59 +37,6 @@ interface ItineraryRepository {
   /** @brief deletes an itinerary */
   fun deleteItinerary(itinerary: Itinerary)
 }
-/**
- * const val ITINERARY_COLLECTION_PATH: String = "itineraries"
- *
- * /** @brief repository implementation using firestore as data source */ class
- * ItineraryRepositoryFirestoreImpl : ItineraryRepository { private val LOG_DEBUG_TAG =
- * "REPOSITORY_FIRESTORE" private val db = FirebaseFirestore.getInstance() private val
- * itineraryCollection = db.collection(ITINERARY_COLLECTION_PATH) /** @return a freshly generated
- * UID for an itinerary */ private fun genItineraryUid(): String = itineraryCollection.document().id
- *
- * override fun getPublicItineraries(): Flow<List<Itinerary>> { return flow { val snapshot =
- * itineraryCollection .whereEqualTo("visible", true) .get() .addOnSuccessListener {
- * Log.d(LOG_DEBUG_TAG, "Get success") } .addOnFailureListener { e -> Log.d(LOG_DEBUG_TAG, "Get
- * failure: err=$e") } .await() val itineraryList = snapshot.map { document ->
- * document.toObject(Itinerary::class.java) } emit(itineraryList) } }
- *
- * override fun getUserItineraries(userUid: String): Flow<List<Itinerary>> { val currUserUid =
- * FirebaseAuth.getInstance().currentUser?.uid
- *
- * return flow {
- *
- * // if the current user is non-null and equal to the queried user, return all itineraries. //
- * otherwise, we only return the public ones val query = if (currUserUid != null && currUserUid ==
- * userUid) { itineraryCollection.whereEqualTo(ItineraryLabels.USER_UID, userUid) } else {
- * itineraryCollection .whereEqualTo(ItineraryLabels.USER_UID, userUid)
- * .whereEqualTo(ItineraryLabels.VISIBLE, true) }
- *
- * val snapshot = query .get() .addOnSuccessListener { Log.d(LOG_DEBUG_TAG, "Get success") }
- * .addOnFailureListener { e -> Log.d(LOG_DEBUG_TAG, "Get failure, err=$e") } .await()
- *
- * val itineraryList = snapshot.map { document -> document.toObject(Itinerary::class.java) }
- * emit(itineraryList) } }
- *
- * override fun getItinerariesWithTags(tags: List<Tag>): Flow<List<Itinerary>> { return flow { val
- * query = itineraryCollection.whereArrayContainsAny(ItineraryLabels.TAGS, tags)
- *
- * val matches = query.get().await().map { document -> document.toObject(Itinerary::class.java) }
- * Log.d(LOG_DEBUG_TAG, "got $matches") emit(matches) } }
- *
- * override fun setItinerary(itinerary: Itinerary) { if (itinerary.uid.isBlank()) { itinerary.uid =
- * genItineraryUid() } val docRef = itineraryCollection.document(itinerary.uid) docRef
- * .set(itinerary.toMap()) .addOnSuccessListener { Log.d(LOG_DEBUG_TAG, "Set success") }
- * .addOnFailureListener { e -> Log.d(LOG_DEBUG_TAG, "Set failure: err=$e") } }
- *
- * override fun updateItinerary(oldUid: String, new: Itinerary) { assert(oldUid == new.uid) val
- * docRef = itineraryCollection.document(oldUid) docRef .update(new) .addOnSuccessListener {
- * Log.d(LOG_DEBUG_TAG, "Update success") } .addOnFailureListener { e -> Log.d(LOG_DEBUG_TAG,
- * "Update failure: err=$e") } }
- *
- * override fun deleteItinerary(itinerary: Itinerary) { val docRef =
- * itineraryCollection.document(itinerary.uid) docRef .delete() .addOnSuccessListener {
- * Log.d(LOG_DEBUG_TAG, "Delete success: uid=$itinerary.uid") } .addOnFailureListener { e ->
- * Log.d(LOG_DEBUG_TAG, "Delete failure: uid=$itinerary.uid. err=$e") } } }
- */
 
 /** @brief repository used for testing viewmodel functionality */
 class ItineraryRepositoryTestImpl : ItineraryRepository {
@@ -137,7 +84,6 @@ class ItineraryRepositoryTestImpl : ItineraryRepository {
 }
 
 class ItineraryRepositoryImpl : ItineraryRepository {
-
   private val db = FirebaseFirestore.getInstance()
   private val itinerariesCollection = db.collection("itineraries")
 
