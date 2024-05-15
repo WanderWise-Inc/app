bed4405812a7cc61e4566a4766768c2a36363557
Oscar Duong <notzeoscar@gmail.com>
2024-05-15 18:55:42 +0200

    Update for new signin

--- app/src/main/java/com/github/wanderwise_inc/app/MainActivity.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/MainActivity.kt
@@ -2,42 +2,34 @@ package com.github.wanderwise_inc.app
 
 import android.Manifest
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.github.wanderwise_inc.app.data.DirectionsRepository
 import com.github.wanderwise_inc.app.data.GoogleSignInLauncher
 import com.github.wanderwise_inc.app.data.ImageRepository
 import com.github.wanderwise_inc.app.data.ItineraryRepository
 import com.github.wanderwise_inc.app.data.ProfileRepository
-import com.github.wanderwise_inc.app.data.SignInRepository
 import com.github.wanderwise_inc.app.di.AppModule
 import com.github.wanderwise_inc.app.ui.navigation.graph.RootNavigationGraph
 import com.github.wanderwise_inc.app.ui.theme.WanderWiseTheme
 import com.github.wanderwise_inc.app.viewmodel.BottomNavigationViewModel
 import com.github.wanderwise_inc.app.viewmodel.CreateItineraryViewModel
 import com.github.wanderwise_inc.app.viewmodel.ItineraryViewModel
+import com.github.wanderwise_inc.app.viewmodel.LoginViewModel
 import com.github.wanderwise_inc.app.viewmodel.ProfileViewModel
-import com.github.wanderwise_inc.app.viewmodel.UserLocationClient
-import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
-    private val appModule by lazy { AppModule(this, navController) }
+    private val appModule by lazy { AppModule(this) }
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var firebaseStorage: FirebaseStorage
@@ -46,11 +38,11 @@ class MainActivity : ComponentActivity() {
     private lateinit var itineraryRepository: ItineraryRepository
     private lateinit var directionsRepository: DirectionsRepository
     private lateinit var profileRepository: ProfileRepository
-    private lateinit var signInRepository: SignInRepository
 
     private lateinit var bottomNavigationViewModel: BottomNavigationViewModel
     private lateinit var createItineraryViewModel: CreateItineraryViewModel
     private lateinit var itineraryViewModel: ItineraryViewModel
+    private lateinit var loginViewModel: LoginViewModel
     private lateinit var profileViewModel: ProfileViewModel
 
     private lateinit var googleSignInLauncher: GoogleSignInLauncher
@@ -70,11 +62,11 @@ class MainActivity : ComponentActivity() {
                 ) {
                     navController = rememberNavController()
                     RootNavigationGraph(
-                        googleSignInLauncher,
                         profileViewModel,
                         itineraryViewModel,
                         createItineraryViewModel,
                         bottomNavigationViewModel,
+                        loginViewModel,
                         imageRepository,
                         navController,
                         firebaseAuth
@@ -112,13 +104,13 @@ class MainActivity : ComponentActivity() {
         itineraryRepository = appModule.itineraryRepository
         directionsRepository = appModule.directionsRepository
         profileRepository = appModule.profileRepository
-        signInRepository = appModule.signInRepository
     }
 
     private fun initializeViewModels() {
         bottomNavigationViewModel = appModule.bottomNavigationViewModel
         createItineraryViewModel = appModule.createItineraryViewModel
         itineraryViewModel = appModule.itineraryViewModel
+        loginViewModel = appModule.loginViewModel
         profileViewModel = appModule.profileViewModel
     }
 }
