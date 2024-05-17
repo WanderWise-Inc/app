80716021bd23157341b157c0fea17f2e3e3a1afc
ThomasPFMuller <thomas.muller@epfl.ch>
2024-05-17 06:41:03 +0300

    ran ktfmt

--- app/src/test/java/com/github/wanderwise_inc/app/ui/map/SearchLocationTest.kt
+++ app/src/test/java/com/github/wanderwise_inc/app/ui/map/SearchLocationTest.kt
@@ -1,26 +1,14 @@
 package com.github.wanderwise_inc.app.ui.map
 
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.lifecycle.liveData
 import androidx.navigation.NavHostController
 import com.github.wanderwise_inc.app.model.location.Itinerary
-import com.github.wanderwise_inc.app.model.location.Location
-import com.github.wanderwise_inc.app.ui.TestTags
 import com.github.wanderwise_inc.app.viewmodel.CreateItineraryViewModel
-import com.google.android.gms.maps.MapsInitializer
-import kotlinx.coroutines.flow.flow
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoRule
 import org.robolectric.RobolectricTestRunner
@@ -44,18 +32,17 @@ class SearchLocationTest {
     }
 
     `when`(createItineraryViewModel.getNewItinerary()).thenReturn(itineraryBuilder)
-    `when`(createItineraryViewModel.fetchPlaces(anyString())).then { *//* do nothing *//*}
+    `when`(createItineraryViewModel.fetchPlaces(anyString())).then { */
+  /* do nothing */
+  /*}
     `when`(createItineraryViewModel.getPlacesLiveData())
         .thenReturn(liveData { emptyList<Location>() })
     `when`(createItineraryViewModel.getUserLocation())
         .thenReturn(flow { emit(Location(lat = 0.0, long = 0.0)) })
   }*/
 
-  @Test
-  fun dummy() {
-    
-  }
-  
+  @Test fun dummy() {}
+
   /*@Test
   fun `initial elements are displayed correctly`() {
     composeTestRule.onNodeWithTag(TestTags.LOCATION_SEARCH_BAR).assertIsDisplayed()
