ba02b65ca1be515db81a3a650642ed1b2ade916c
engjellismaili <engjell.ismaili@epfl.ch>
2024-05-17 16:21:23 +0200

    Modified ProfilePicture to use Coil and new Implementation

--- app/src/main/java/com/github/wanderwise_inc/app/ui/profile/ProfilePicture.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/ui/profile/ProfilePicture.kt
@@ -1,6 +1,7 @@
 package com.github.wanderwise_inc.app.ui.profile
 
 import androidx.compose.foundation.Image
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -9,7 +10,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.github.wanderwise_inc.app.R
 import com.github.wanderwise_inc.app.model.profile.Profile
 import com.github.wanderwise_inc.app.viewmodel.ProfileViewModel
@@ -19,19 +25,23 @@ import kotlinx.coroutines.flow.flow
 @Composable
 fun ProfilePicture(profile: Profile?, profileViewModel: ProfileViewModel, modifier: Modifier) {
   // calling `remember` limits recomposition to `Profile` changes
-  val defaultProfilePictureFlow = remember(profile) { profileViewModel.getDefaultProfilePicture() }
   val profilePictureFlow =
       remember(profile) {
         if (profile != null) profileViewModel.getProfilePicture(profile) else flow { emit(null) }
       }
 
-  val defaultProfilePicture by defaultProfilePictureFlow.collectAsState(initial = null)
   val profilePicture by profilePictureFlow.collectAsState(initial = null)
-
-  val painter: Painter =
-      if (profilePicture != null) BitmapPainter(profilePicture!!.asImageBitmap())
-      else if (defaultProfilePicture != null) BitmapPainter(defaultProfilePicture!!.asImageBitmap())
-      else painterResource(id = R.drawable.profile_icon)
-
-  Image(painter = painter, contentDescription = "profile_icon", modifier = modifier)
+    val painter = rememberAsyncImagePainter(model = ImageRequest.Builder(LocalContext.current).data(profilePicture).build())
+    if (painter.state is AsyncImagePainter.State.Error) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_icon),
+            contentDescription = "profile_icon",
+            modifier = modifier
+        )
+    } else {
+        Image(painter = painter, contentDescription = "profile_icon", modifier = modifier)
+        if (painter.state is AsyncImagePainter.State.Loading) {
+            CircularProgressIndicator()
+        }
+    }
 }
