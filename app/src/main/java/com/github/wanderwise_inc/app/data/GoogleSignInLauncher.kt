6e165ea54121c0bc4a45a0337ab4edad29ff3ec0
Oscar Duong <notzeoscar@gmail.com>
2024-05-17 23:12:50 +0200

    Remove providers from GoogleSignInLauncher

--- app/src/main/java/com/github/wanderwise_inc/app/data/GoogleSignInLauncher.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/data/GoogleSignInLauncher.kt
@@ -3,17 +3,28 @@ package com.github.wanderwise_inc.app.data
 import android.content.Intent
 import androidx.activity.result.ActivityResultLauncher
 import com.firebase.ui.auth.AuthUI
-import com.github.wanderwise_inc.app.di.SignInProviders
 
 class GoogleSignInLauncher(
     private val activityResultLauncher: ActivityResultLauncher<Intent>,
-    private val providers: List<AuthUI.IdpConfig> = SignInProviders.providers
 ) : SignInLauncher {
-  private val signInIntent by lazy {
-    AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
-  }
+    private val providers by lazy {
+        listOf(
+            AuthUI
+                .IdpConfig
+                .GoogleBuilder()
+                .build()
+        )
+    }
 
-  override fun signIn() {
-    activityResultLauncher.launch(signInIntent)
-  }
+    private val signInIntent by lazy {
+        AuthUI
+            .getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+    }
+
+    override fun signIn() {
+        activityResultLauncher.launch(signInIntent)
+    }
 }
