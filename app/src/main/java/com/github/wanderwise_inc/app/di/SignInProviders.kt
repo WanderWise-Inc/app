4cfdd92a05be4a7291100a5c03140353501e350d
Oscar Duong <notzeoscar@gmail.com>
2024-05-17 23:21:05 +0200

    Remove SignInProviders not used anymore

--- app/src/main/java/com/github/wanderwise_inc/app/di/SignInProviders.kt
+++
@@ -1,9 +0,0 @@
-package com.github.wanderwise_inc.app.di
-
-import com.firebase.ui.auth.AuthUI
-
-object SignInProviders {
-  val providers by lazy { listOf(google) }
-
-  val google by lazy { AuthUI.IdpConfig.GoogleBuilder().build() }
-}
