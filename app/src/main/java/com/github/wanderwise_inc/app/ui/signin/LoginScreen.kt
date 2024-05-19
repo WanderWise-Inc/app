c99a21d6b8c5d39ff4d273b6ca2ec06217825b9a
imaneb942 <imane.benkamoun@epfl.ch>
2024-05-19 15:44:49 +0200

    formatted + removed comments

--- app/src/main/java/com/github/wanderwise_inc/app/ui/signin/LoginScreen.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/ui/signin/LoginScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.*
 import androidx.compose.animation.core.*
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -12,7 +11,6 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
