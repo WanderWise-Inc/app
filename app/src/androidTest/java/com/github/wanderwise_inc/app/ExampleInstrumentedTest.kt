56ed0eb8956f954fe2c491f0cba05e5ece08428d
ThomasPFMuller <thomas.muller@epfl.ch>
2024-05-19 17:02:35 +0300

    removing SearchBarTest

--- app/src/androidTest/java/com/github/wanderwise_inc/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/wanderwise_inc/app/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.github.wanderwise_inc.app.di
+package com.github.wanderwise_inc.app
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
