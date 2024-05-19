09a98ab1bd33c5148acce026247f87e515fbd62e
Oscar Duong <notzeoscar@gmail.com>
2024-05-19 21:55:58 +0200

    Move util function

---
+++ app/src/androidTest/java/com/github/wanderwise_inc/app/e2e/E2EUtils.kt
@@ -0,0 +1,20 @@
+package com.github.wanderwise_inc.app.e2e
+
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.semantics.getOrNull
+import androidx.compose.ui.test.SemanticsMatcher
+
+object E2EUtils {
+
+    /**
+     * This function checks if a given subTestTag is present in the TestTag of a node.
+     *
+     * @param subTestTag The subTestTag to check for in the TestTag of a node.
+     * @return SemanticsMatcher A SemanticsMatcher that can be used to find nodes that contain the given subTestTag in their TestTag.
+     */
+    fun hasSubTestTag(subTestTag: String): SemanticsMatcher {
+        return SemanticsMatcher("Node with subtag $subTestTag") { node ->
+            node.config.getOrNull(SemanticsProperties.TestTag)?.contains(subTestTag) == true
+        }
+    }
+}
--- app/src/androidTest/java/com/github/wanderwise_inc/app/e2e/LikeE2ETest.kt
+++ app/src/androidTest/java/com/github/wanderwise_inc/app/e2e/LikeE2ETest.kt
@@ -21,83 +21,86 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class LikeE2ETest {
 
-  @get:Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-  @get:Rule
-  val grantPermissionRule: GrantPermissionRule =
-      GrantPermissionRule.grant(
-          android.Manifest.permission.ACCESS_FINE_LOCATION,
-          android.Manifest.permission.ACCESS_COARSE_LOCATION)
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
 
-  @Test
-  fun likeFlowTest() {
-    // Click on the sign in button
-    composeTestRule.onNodeWithTag(TestTags.SIGN_IN_BUTTON).performClick()
-    composeTestRule.waitUntil {
-      composeTestRule.onNodeWithTag(TestTags.OVERVIEW_SCREEN).isDisplayed()
-    }
+    @Test
+    fun likeFlowTest() {
+        // Click on the sign in button
+        composeTestRule.onNodeWithTag(TestTags.SIGN_IN_BUTTON).performClick()
+        composeTestRule.waitUntil {
+            composeTestRule.onNodeWithTag(TestTags.OVERVIEW_SCREEN).isDisplayed()
+        }
 
-    // Find first like button
-    val firstLikeButton =
-        composeTestRule.onAllNodes(hasSubTestTag(TestTags.ITINERARY_BANNER_LIKE_BUTTON)).onFirst()
+        // Find first like button
+        val firstLikeButton =
+            composeTestRule.onAllNodes(E2EUtils.hasSubTestTag(TestTags.ITINERARY_BANNER_LIKE_BUTTON))
+                .onFirst()
 
-    // Click on the first like button to like the itinerary
-    firstLikeButton.assertIsDisplayed()
-    firstLikeButton.performClick()
-    composeTestRule.waitUntil {
-      composeTestRule.onNodeWithTag(TestTags.OVERVIEW_SCREEN).isDisplayed()
-    }
+        // Click on the first like button to like the itinerary
+        firstLikeButton.assertIsDisplayed()
+        firstLikeButton.performClick()
+        composeTestRule.waitUntil {
+            composeTestRule.onNodeWithTag(TestTags.OVERVIEW_SCREEN).isDisplayed()
+        }
 
-    // Click on the bottom navigation bar liked button to navigate to LikedScreen
-    composeTestRule
-        .onNodeWithTag(testTag = TestTags.BOTTOM_NAV_LIKED, useUnmergedTree = true)
-        .performClick()
-    composeTestRule.waitUntil { composeTestRule.onNodeWithTag(TestTags.LIKED_SCREEN).isDisplayed() }
-
-    // Check if the liked itinerary banner is displayed
-    composeTestRule
-        .onAllNodes(hasSubTestTag(TestTags.ITINERARY_BANNER))
-        .onFirst()
-        .assertIsDisplayed()
-
-    // Check if the liked itinerary banner like button is displayed
-    val likedItineraryLikeButton =
+        // Click on the bottom navigation bar liked button to navigate to LikedScreen
         composeTestRule
-            .onAllNodes(hasSubTestTag(TestTags.ITINERARY_BANNER_LIKE_BUTTON))
+            .onNodeWithTag(testTag = TestTags.BOTTOM_NAV_LIKED, useUnmergedTree = true)
+            .performClick()
+        composeTestRule.waitUntil {
+            composeTestRule.onNodeWithTag(TestTags.LIKED_SCREEN).isDisplayed()
+        }
+
+        // Check if the liked itinerary banner is displayed
+        composeTestRule
+            .onAllNodes(E2EUtils.hasSubTestTag(TestTags.ITINERARY_BANNER))
             .onFirst()
             .assertIsDisplayed()
 
-    // Click on the liked itinerary banner like button to remove the like from the itinerary
-    likedItineraryLikeButton.performClick()
-    composeTestRule.waitUntil { composeTestRule.onNodeWithTag(TestTags.LIKED_SCREEN).isDisplayed() }
+        // Check if the liked itinerary banner like button is displayed
+        val likedItineraryLikeButton =
+            composeTestRule
+                .onAllNodes(E2EUtils.hasSubTestTag(TestTags.ITINERARY_BANNER_LIKE_BUTTON))
+                .onFirst()
+                .assertIsDisplayed()
 
-    // Click on the bottom navigation bar overview button to navigate back to OverviewScreen
-    composeTestRule
-        .onNodeWithTag(testTag = TestTags.BOTTOM_NAV_OVERVIEW, useUnmergedTree = true)
-        .performClick()
-    composeTestRule.waitUntil {
-      composeTestRule.onNodeWithTag(TestTags.OVERVIEW_SCREEN).isDisplayed()
+        // Click on the liked itinerary banner like button to remove the like from the itinerary
+        likedItineraryLikeButton.performClick()
+        composeTestRule.waitUntil {
+            composeTestRule.onNodeWithTag(TestTags.LIKED_SCREEN).isDisplayed()
+        }
+
+        // Click on the bottom navigation bar overview button to navigate back to OverviewScreen
+        composeTestRule
+            .onNodeWithTag(testTag = TestTags.BOTTOM_NAV_OVERVIEW, useUnmergedTree = true)
+            .performClick()
+        composeTestRule.waitUntil {
+            composeTestRule.onNodeWithTag(TestTags.OVERVIEW_SCREEN).isDisplayed()
+        }
+
+        // Click on the bottom navigation bar liked button to navigate to LikedScreen
+        composeTestRule
+            .onNodeWithTag(testTag = TestTags.BOTTOM_NAV_LIKED, useUnmergedTree = true)
+            .performClick()
+        composeTestRule.waitUntil {
+            composeTestRule.onNodeWithTag(TestTags.LIKED_SCREEN).isDisplayed()
+        }
+
+        // Check if no itinerary banner is displayed
+        composeTestRule
+            .onAllNodes(E2EUtils.hasSubTestTag(TestTags.ITINERARY_BANNER))
+            .onFirst()
+            .assertDoesNotExist()
     }
-
-    // Click on the bottom navigation bar liked button to navigate to LikedScreen
-    composeTestRule
-        .onNodeWithTag(testTag = TestTags.BOTTOM_NAV_LIKED, useUnmergedTree = true)
-        .performClick()
-    composeTestRule.waitUntil { composeTestRule.onNodeWithTag(TestTags.LIKED_SCREEN).isDisplayed() }
-
-    // Check if no itinerary banner is displayed
-    composeTestRule
-        .onAllNodes(hasSubTestTag(TestTags.ITINERARY_BANNER))
-        .onFirst()
-        .assertDoesNotExist()
-  }
-
-  // Function to check if the node has a testTag which contains the given subTestTag
-  private fun hasSubTestTag(subTestTag: String): SemanticsMatcher {
-    return SemanticsMatcher("Node with subtag $subTestTag") { node ->
-      node.config.getOrNull(SemanticsProperties.TestTag)?.contains(subTestTag) == true
-    }
-  }
 }
