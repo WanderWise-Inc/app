bcfeb9530516855801cc27786cfc7e8d05bef756
Oscar Duong <notzeoscar@gmail.com>
2024-05-19 14:53:24 +0200

    ...

--- app/src/androidTest/java/com/github/wanderwise_inc/app/e2e/LikeE2ETest.kt
+++ app/src/androidTest/java/com/github/wanderwise_inc/app/e2e/LikeE2ETest.kt
@@ -1,13 +1,10 @@
 package com.github.wanderwise_inc.app.e2e
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import com.github.wanderwise_inc.app.MainActivity
-import com.github.wanderwise_inc.app.ui.TestTags
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,20 +12,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class LikeE2ETest {
 
-    @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+  @get:Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
-    @get:Rule
-    val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
+  @get:Rule val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
 
-    @get:Rule
-    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        android.Manifest.permission.ACCESS_FINE_LOCATION,
-        android.Manifest.permission.ACCESS_COARSE_LOCATION
-    )
+  @get:Rule
+  val grantPermissionRule: GrantPermissionRule =
+      GrantPermissionRule.grant(
+          android.Manifest.permission.ACCESS_FINE_LOCATION,
+          android.Manifest.permission.ACCESS_COARSE_LOCATION)
 
-    @Test
-    fun likeFlowTest() {
-
-    }
+  @Test fun likeFlowTest() {}
 }
