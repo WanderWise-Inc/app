3e2257b009181fe622f65028cbd5b4ddd1467195
balloux <leonard.balloux@epfl.ch>
2024-05-27 17:25:30 +0200

    small modifications

--- app/src/main/java/com/github/wanderwise_inc/app/ui/creation/steps/CreationStepChooseTags.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/ui/creation/steps/CreationStepChooseTags.kt
@@ -14,23 +14,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.ChipDefaults
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -50,7 +42,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupPositionProvider
 import androidx.core.text.isDigitsOnly
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -72,10 +63,9 @@ fun CreationStepChooseTagsScreen(
   Column(
       horizontalAlignment = Alignment.CenterHorizontally,
       modifier =
-      Modifier
-          .fillMaxSize()
-          .testTag(TestTags.CREATION_SCREEN_TAGS)
-          .background(MaterialTheme.colorScheme.background),
+          Modifier.fillMaxSize()
+              .testTag(TestTags.CREATION_SCREEN_TAGS)
+              .background(MaterialTheme.colorScheme.background),
       verticalArrangement = Arrangement.spacedBy(10.dp)) {
         ItineraryImageBanner(
             createItineraryViewModel = createItineraryViewModel,
@@ -101,21 +91,20 @@ fun ItineraryImageBanner(
 
   Box(
       modifier =
-      Modifier
-          .fillMaxWidth()
-          .testTag(TestTags.CREATION_SCREEN_IMAGE_BANNER_BOX)
-          .padding(all = 10.dp)
-          .height(120.dp)
-          .clip(MaterialTheme.shapes.medium)
-          .background(MaterialTheme.colorScheme.surface)
-          .clickable {
-              coroutineScope.launch {
+          Modifier.fillMaxWidth()
+              .testTag(TestTags.CREATION_SCREEN_IMAGE_BANNER_BOX)
+              .padding(all = 10.dp)
+              .height(120.dp)
+              .clip(MaterialTheme.shapes.medium)
+              .background(MaterialTheme.colorScheme.surface)
+              .clickable {
+                coroutineScope.launch {
                   Intent(Intent.ACTION_GET_CONTENT).also {
-                      it.type = "image/*" // Set type to any image format.
-                      imageRepository.launchActivity(it) // Launch activity to select an image.
+                    it.type = "image/*" // Set type to any image format.
+                    imageRepository.launchActivity(it) // Launch activity to select an image.
                   }
-              }
-          },
+                }
+              },
       contentAlignment = Alignment.Center) {
         imageUploaded = imageRepository.getCurrentFile()
         if (imageUploaded != null) {
@@ -128,9 +117,7 @@ fun ItineraryImageBanner(
                       .build(),
               contentDescription = "itinerary_image",
               contentScale = ContentScale.Crop,
-              modifier = Modifier
-                  .testTag(TestTags.CREATION_SCREEN_IMAGE_BANNER)
-                  .fillMaxSize())
+              modifier = Modifier.testTag(TestTags.CREATION_SCREEN_IMAGE_BANNER).fillMaxSize())
         } else {
           Text("Itinerary Banner Please Upload Image")
         }
@@ -201,44 +188,22 @@ fun RelevantTags(createItineraryViewModel: CreateItineraryViewModel) {
 
   Button(
       modifier = Modifier.testTag(TestTags.ITINERARY_CREATION_TAGS),
-      onClick = { isTagsDDM = true}) {
+      onClick = { isTagsDDM = true }) {
         Text("Add Tags")
       }
 
-    if(isTagsDDM) {
-        Popup(
-            alignment = Alignment.Center,
-            onDismissRequest = {isTagsDDM = false}
-        ){
-            OutlinedCard(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .wrapContentSize(),
-//                elevation = 8.dp,
-                shape = MaterialTheme.shapes.medium
-            ){
-                TagSelector(searchCategoryList = allTags, selectedTags = dispTags, createItineraryViewModel = createItineraryViewModel)
-            }
-        }
+  if (isTagsDDM) {
+    Popup(alignment = Alignment.Center, onDismissRequest = { isTagsDDM = false }) {
+      OutlinedCard(
+          modifier = Modifier.padding(8.dp).wrapContentSize(),
+          shape = MaterialTheme.shapes.medium) {
+            TagSelector(
+                searchCategoryList = allTags,
+                selectedTags = dispTags,
+                createItineraryViewModel = createItineraryViewModel)
+          }
     }
-
-//  DropdownMenu(expanded = isTagsDDM, onDismissRequest = { isTagsDDM = false }) {
-//    allTags.forEach { tag ->
-//      DropdownMenuItem(
-//          text = { Text(tag) },
-//          modifier = Modifier.testTag("${TestTags.ITINERARY_CREATION_TAGS}_$tag"),
-//          onClick = {
-//            isTagsDDM = false
-//            if (!selectedTags.contains(tag) && selectedTags.size < 3) {
-//              selectedTags.add(tag)
-//              createItineraryViewModel.getNewItinerary()!!.tags.clear()
-//              createItineraryViewModel.getNewItinerary()!!.tags.addAll(selectedTags)
-//            } else if (selectedTags.size >= 3) {
-//              triedToAddMoreThan3Tags = true
-//            }
-//          })
-//    }
-//  }
+  }
 }
 
 @Composable
@@ -263,58 +228,59 @@ fun IsPublicSwitchButton(createItineraryViewModel: CreateItineraryViewModel) {
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun TagSelector(searchCategoryList: List<SearchCategory>, selectedTags: MutableList<Tag>, createItineraryViewModel: CreateItineraryViewModel){
-    Column {
-        Box(Modifier.fillMaxWidth().padding(6.dp)) {
-            Text("Please select up to $MAX_TAGS tags", Modifier.align(Alignment.Center))
-        }
-        FlowRow(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(12.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(0.dp)
-        ) {
-            for (sc in searchCategoryList) {
-                TagsButton(
-                    searchCategory = sc,
-                    selectedTags = selectedTags,
-                    createItineraryViewModel = createItineraryViewModel
-                )
-            }
-        }
+fun TagSelector(
+    searchCategoryList: List<SearchCategory>,
+    selectedTags: MutableList<Tag>,
+    createItineraryViewModel: CreateItineraryViewModel
+) {
+  Column {
+    Box(Modifier.fillMaxWidth().padding(6.dp)) {
+      Text("Please select up to $MAX_TAGS tags", Modifier.align(Alignment.Center))
     }
+    FlowRow(
+        modifier = Modifier.fillMaxWidth().padding(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(0.dp)) {
+          for (sc in searchCategoryList) {
+            TagsButton(
+                searchCategory = sc,
+                selectedTags = selectedTags,
+                createItineraryViewModel = createItineraryViewModel)
+          }
+        }
+  }
 }
 
 @Composable
-fun TagsButton(searchCategory: SearchCategory, selectedTags: MutableList<Tag>, createItineraryViewModel: CreateItineraryViewModel){
-    var selected by  remember{ mutableStateOf(selectedTags.contains(searchCategory.tag))}
-    FilterChip(
-        modifier = Modifier.wrapContentSize(),
-        selected = selected,
-        onClick = {
-            if(selected){
-                selectedTags.remove(searchCategory.tag)
-                createItineraryViewModel.getNewItinerary()!!.tags.remove(searchCategory.tag)
-                selected = false
-            } else if(selectedTags.size < MAX_TAGS){
-                selectedTags.add(searchCategory.tag)
-                createItineraryViewModel.getNewItinerary()!!.tags.add(searchCategory.tag)
-                selected = true
-            } },
-        label = { Text(searchCategory.tag) },
-        leadingIcon = {
-            Icon(imageVector = searchCategory.icon, searchCategory.tag)
-        },
-        colors = FilterChipDefaults.filterChipColors(
-            containerColor = MaterialTheme.colorScheme.secondaryContainer,
-            labelColor = MaterialTheme.colorScheme.onSecondaryContainer,
-            iconColor = MaterialTheme.colorScheme.onSecondaryContainer,
-            selectedContainerColor = MaterialTheme.colorScheme.primaryContainer,
-            selectedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            selectedLeadingIconColor = MaterialTheme.colorScheme.onPrimaryContainer,)
-    )
-
+fun TagsButton(
+    searchCategory: SearchCategory,
+    selectedTags: MutableList<Tag>,
+    createItineraryViewModel: CreateItineraryViewModel
+) {
+  var selected by remember { mutableStateOf(selectedTags.contains(searchCategory.tag)) }
+  FilterChip(
+      modifier = Modifier.wrapContentSize(),
+      selected = selected,
+      onClick = {
+        if (selected) {
+          selectedTags.remove(searchCategory.tag)
+          createItineraryViewModel.getNewItinerary()!!.tags.remove(searchCategory.tag)
+          selected = false
+        } else if (selectedTags.size < MAX_TAGS) {
+          selectedTags.add(searchCategory.tag)
+          createItineraryViewModel.getNewItinerary()!!.tags.add(searchCategory.tag)
+          selected = true
+        }
+      },
+      label = { Text(searchCategory.tag) },
+      leadingIcon = { Icon(imageVector = searchCategory.icon, searchCategory.tag) },
+      colors =
+          FilterChipDefaults.filterChipColors(
+              containerColor = MaterialTheme.colorScheme.secondaryContainer,
+              labelColor = MaterialTheme.colorScheme.onSecondaryContainer,
+              iconColor = MaterialTheme.colorScheme.onSecondaryContainer,
+              selectedContainerColor = MaterialTheme.colorScheme.primaryContainer,
+              selectedLabelColor = MaterialTheme.colorScheme.onPrimaryContainer,
+              selectedLeadingIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+          ))
 }
-
-
