131a926d5d892c4003368eb41c2b073b1ebee54a
Oscar Duong <notzeoscar@gmail.com>
2024-06-01 16:23:12 +0200

    Comment

--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationClient.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationClient.kt
@@ -3,18 +3,31 @@ package com.github.wanderwise_inc.app.viewmodel
 import com.github.wanderwise_inc.app.model.location.Location
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * `LocationClient` is an interface that defines the contract for classes that provide location.
+ *
+ * This interface should be implemented by classes that provide location updates.
+ */
 interface LocationClient {
 
-  /**
-   * @param interval the frequency of the updates in ms
-   * @return a flow containing the updated locations
-   * @brief get the updated location
-   */
-  fun getLocationUpdates(interval: Long): Flow<Location>
+    /**
+     * Gets location updates at the specified interval.
+     *
+     * This method returns a Flow that emits Location objects.
+     * Each Location object represents the device's location at a particular point in time.
+     * The updates are emitted at the interval specified by the `interval` parameter.
+     *
+     * @param interval The interval at which location updates are desired, in milliseconds.
+     * @return A Flow that emits Location objects at the specified interval.
+     */
+    fun getLocationUpdates(interval: Long): Flow<Location>
 
-  /**
-   * @param message the description of the exception
-   * @brief exception class for error with the LocationClient
-   */
-  class LocationException(message: String) : Exception()
+    /**
+     * `LocationException` is a class that represents exceptions related to location updates.
+     *
+     * This class extends the `Exception` class and is used to indicate that an error occurred while getting location updates.
+     *
+     * @param message The detail message string of this throwable.
+     */
+    class LocationException(message: String) : Exception()
 }
--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/UserLocationClient.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/UserLocationClient.kt
@@ -16,61 +16,72 @@ import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 
 /**
- * @param context the context used for retrieving system services and checking permissions.
- * @param client the FusedLocationProviderClient instance for requesting location updates.
- * @brief client responsible for retrieving user location updates using the Fused Location Provider
- *   API.
+ * `UserLocationClient` is a class that provides location updates.
+ *
+ * This class implements the `LocationClient` interface and uses the `FusedLocationProviderClient` to get location updates.
+ *
+ * @property context The context in which the `UserLocationClient` is being used.
+ * @property client The `FusedLocationProviderClient` used to get location updates.
+ * @constructor Creates a `UserLocationClient` with the specified context and client.
+
  */
 class UserLocationClient(
     private val context: Context,
     private val client: FusedLocationProviderClient
 ) : LocationClient {
 
-  /**
-   * @param interval the interval, in milliseconds, at which location updates are requested.
-   * @return a flow emitting the user's location updates.
-   * @throws IllegalArgumentException if interval is not strictly positive
-   * @throws LocationClient.LocationException if location permissions are denied or if there's no
-   *   means of tracking position (e.g., GPS and network providers are disabled).
-   * @brief requests location updates at a specified interval.
-   */
-  @SuppressLint("MissingPermission")
-  override fun getLocationUpdates(interval: Long): Flow<Location> {
-    if (interval <= 0) {
-      throw IllegalArgumentException("Interval not strictly positive: interval=$interval")
-    }
+    /**
+     * Gets location updates at the specified interval.
+     *
+     * This method returns a Flow that emits Location objects. Each Location object represents the device's location at a particular point in time.
+     * The updates are emitted at the interval specified by the `interval` parameter.
+     *
+     * @param interval The interval at which location updates are desired, in milliseconds.
+     * @return A Flow that emits Location objects at the specified interval.
+     * @throws IllegalArgumentException If the interval is not strictly positive.
+     * @throws LocationClient.LocationException If location permissions are denied or if there is no mean of tracking position.
+     */
+    @SuppressLint("MissingPermission")
+    override fun getLocationUpdates(interval: Long): Flow<Location> {
+        if (interval <= 0) {
+            throw IllegalArgumentException("Interval not strictly positive: interval=$interval")
+        }
 
-    return callbackFlow {
-      if (!context.hasLocationPermission()) {
-        throw LocationClient.LocationException("Location permissions denied")
-      }
-
-      val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-      val isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-      val isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-      if (!isGPSEnabled && !isNetworkEnabled) {
-        throw LocationClient.LocationException("No mean of tracking position")
-      }
-
-      val request = LocationRequest.Builder(interval).build()
-
-      val locationCallback =
-          object : LocationCallback() {
-            override fun onLocationResult(result: LocationResult) {
-              super.onLocationResult(result)
-              result.locations.lastOrNull()?.let { androidLocation ->
-                launch {
-                  send(
-                      Location.placeLocation(
-                          LatLng(androidLocation.latitude, androidLocation.longitude)))
-                }
-              }
+        return callbackFlow {
+            if (!context.hasLocationPermission()) {
+                throw LocationClient.LocationException("Location permissions denied")
             }
-          }
 
-      client.requestLocationUpdates(request, locationCallback, Looper.getMainLooper())
+            val locationManager =
+                context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            val isNetworkEnabled =
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                throw LocationClient.LocationException("No mean of tracking position")
+            }
 
-      awaitClose { client.removeLocationUpdates(locationCallback) }
+            val request = LocationRequest.Builder(interval).build()
+
+            val locationCallback =
+                object : LocationCallback() {
+                    override fun onLocationResult(result: LocationResult) {
+                        super.onLocationResult(result)
+                        result.locations.lastOrNull()?.let { androidLocation ->
+                            launch {
+                                send(
+                                    Location.placeLocation(
+                                        LatLng(androidLocation.latitude, androidLocation.longitude)
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+
+            client.requestLocationUpdates(request, locationCallback, Looper.getMainLooper())
+
+            awaitClose { client.removeLocationUpdates(locationCallback) }
+        }
     }
-  }
 }
