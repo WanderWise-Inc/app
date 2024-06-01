124d5f0ad0a28d5f3a222312f3ba38281365e632
Oscar Duong <notzeoscar@gmail.com>
2024-06-01 15:35:52 +0200

    Comment

--- app/src/main/java/com/github/wanderwise_inc/app/data/GoogleSignInLauncher.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/data/GoogleSignInLauncher.kt
@@ -4,16 +4,23 @@ import android.content.Intent
 import androidx.activity.result.ActivityResultLauncher
 import com.firebase.ui.auth.AuthUI
 
+/**
+ * `GoogleSignInLauncher` is a class that implements the `SignInLauncher` interface to handle Google sign-in.
+ *
+ * This class is responsible for initiating the sign-in process using Google as the identity provider.
+ */
 class GoogleSignInLauncher(
     private val activityResultLauncher: ActivityResultLauncher<Intent>,
 ) : SignInLauncher {
-  private val providers by lazy { listOf(AuthUI.IdpConfig.GoogleBuilder().build()) }
+    // `providers` is a list of identity providers. In this case, it only contains Google.
+    private val providers by lazy { listOf(AuthUI.IdpConfig.GoogleBuilder().build()) }
 
-  private val signInIntent by lazy {
-    AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
-  }
+    // `signInIntent` is an intent that is used to initiate the sign-in process.
+    private val signInIntent by lazy {
+        AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
+    }
 
-  override fun signIn() {
-    activityResultLauncher.launch(signInIntent)
-  }
+    override fun signIn() {
+        activityResultLauncher.launch(signInIntent)
+    }
 }
--- app/src/main/java/com/github/wanderwise_inc/app/data/SignInLauncher.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/data/SignInLauncher.kt
@@ -1,5 +1,13 @@
 package com.github.wanderwise_inc.app.data
 
+/**
+ * `SignInLauncher` is an interface that represents the contract for signing in.
+ *
+ * This interface should be implemented by any class that is responsible for handling the sign-in process.
+ */
 interface SignInLauncher {
-  fun signIn()
+    /**
+     * Initiate the sign-in process.
+     */
+    fun signIn()
 }
