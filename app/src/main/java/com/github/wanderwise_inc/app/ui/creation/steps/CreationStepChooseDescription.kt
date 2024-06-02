d77eb1d53cc862c37be0f736ae1934ca87fceb7b
imaneb942 <imane.benkamoun@epfl.ch>
2024-06-02 18:06:59 +0200

    changed the Divider import to material 3

--- app/src/main/java/com/github/wanderwise_inc/app/ui/creation/steps/CreationStepChooseDescription.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/ui/creation/steps/CreationStepChooseDescription.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Divider
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
