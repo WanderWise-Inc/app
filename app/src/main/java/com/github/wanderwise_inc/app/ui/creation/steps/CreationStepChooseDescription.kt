131fcd689c812b9b296f5b047b67cf488cc45ea7
imaneb942 <imane.benkamoun@epfl.ch>
2024-06-02 15:29:31 +0200

    updated the description window

--- app/src/main/java/com/github/wanderwise_inc/app/ui/creation/steps/CreationStepChooseDescription.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/ui/creation/steps/CreationStepChooseDescription.kt
@@ -2,11 +2,16 @@ package com.github.wanderwise_inc.app.ui.creation.steps
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -17,6 +22,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
@@ -37,15 +43,28 @@ fun CreationStepChooseDescriptionScreen(createItineraryViewModel: CreateItinerar
     var validTitle by remember { mutableStateOf(true) }
     Column {
         Text(
-            text = "Add your title and description of your itinerary",
-            style = TextStyle(fontStyle = FontStyle.Italic),
-            modifier = Modifier.padding(8.dp)
+            text = "Add your itinerary title and description",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            softWrap = true,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(bottom = 20.dp, top = 20.dp, start = 16.dp)
         )
-        Divider(color = Color.Gray, thickness = 1.dp)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Box(modifier = Modifier.width(350.dp)
+                .shadow(elevation = 6.dp, shape = RoundedCornerShape(4.dp))) {
+                Divider(color = Color.Gray, thickness = 3.dp)
+            }
+        }
     LazyColumn(
         modifier = Modifier
             .padding(16.dp)
             .background(color = MaterialTheme.colorScheme.background)
+
             .testTag(TestTags.CREATION_SCREEN_DESCRIPTION_TITLE),
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(24.dp)
@@ -84,7 +103,9 @@ fun CreationStepChooseDescriptionScreen(createItineraryViewModel: CreateItinerar
                             fontSize = 18.sp,
                             color = Color.Gray
                         )
-                    }
+                    },
+                    shape = RoundedCornerShape(10.dp)
+
                 )
                 if (!validTitle) {
                     Text(
@@ -122,7 +143,8 @@ fun CreationStepChooseDescriptionScreen(createItineraryViewModel: CreateItinerar
                             fontSize = 18.sp,
                             color = Color.Gray
                         )
-                    }
+                    },
+                    shape = RoundedCornerShape(10.dp)
                 )
             }
         }
