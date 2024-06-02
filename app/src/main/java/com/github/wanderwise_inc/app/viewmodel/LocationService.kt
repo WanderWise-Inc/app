6d3ccc534c693424d05e1330931223d3659a0ef2
Oscar Duong <notzeoscar@gmail.com>
2024-06-02 16:42:35 +0200

    Implement location service to show notification

--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationService.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationService.kt
@@ -3,10 +3,56 @@ package com.github.wanderwise_inc.app.viewmodel
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.github.wanderwise_inc.app.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 
 class LocationService : Service() {
 
-  override fun onBind(intent: Intent?): IBinder? {
-    TODO("Not yet implemented")
-  }
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            ACTION_START -> start()
+            ACTION_STOP -> stop()
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start() {
+        val notification = NotificationCompat.Builder(this, "location")
+            .setContentTitle("Wanderwise tracking")
+            .setContentText("Running")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setOngoing(true)
+
+        startForeground(1, notification.build())
+    }
+
+    private fun stop() {
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    companion object {
+        const val ACTION_START = "ACTION_START"
+        const val ACTION_STOP = "ACTION_STOP"
+    }
 }
