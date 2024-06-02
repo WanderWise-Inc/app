c7afc4eeedae7584381fad6f2a76a1e2066640c4
Oscar Duong <notzeoscar@gmail.com>
2024-06-02 21:41:13 +0200

    Add factory

--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/BottomNavigationViewModel.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/BottomNavigationViewModel.kt
@@ -2,41 +2,42 @@ package com.github.wanderwise_inc.app.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
 /** Enum representing the items in the bottom navigation bar. */
 enum class NavigationItem {
-  OVERVIEW,
-  LIKED,
-  CREATE,
-  MAP,
-  PROFILE
+    OVERVIEW,
+    LIKED,
+    CREATE,
+    MAP,
+    PROFILE
 }
 
 /** ViewModel for managing the state of the bottom navigation bar. */
-class BottomNavigationViewModel {
-  /** LiveData for the selected navigation item index. */
-  private val _selected = MutableLiveData<Int>()
+class BottomNavigationViewModel: ViewModel() {
+    /** LiveData for the selected navigation item index. */
+    private val _selected = MutableLiveData<Int>()
 
-  /**
-   * Publicly exposed LiveData for the selected navigation item index.
-   *
-   * @return the LiveData containing the index of the selected navigation item.
-   */
-  public val selected: LiveData<Int>
-    get() = _selected
+    /**
+     * Publicly exposed LiveData for the selected navigation item index.
+     *
+     * @return the LiveData containing the index of the selected navigation item.
+     */
+    val selected: LiveData<Int>
+        get() = _selected
 
-  /** Initializes the ViewModel with a default selected navigation item index. */
-  init {
-    _selected.value = 0
-    // Initialize with default value
-  }
+    /** Initializes the ViewModel with a default selected navigation item index. */
+    init {
+        _selected.value = 0
+        // Initialize with default value
+    }
 
-  /**
-   * Sets the selected navigation item index.
-   *
-   * @param index the index of the navigation item to select.
-   */
-  fun setSelected(index: Int) {
-    _selected.value = index
-  }
+    /**
+     * Sets the selected navigation item index.
+     *
+     * @param index the index of the navigation item to select.
+     */
+    fun setSelected(index: Int) {
+        _selected.value = index
+    }
 }
--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LoginViewModel.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.github.wanderwise_inc.app.data.ImageRepository
+import com.github.wanderwise_inc.app.data.ProfileRepository
 import com.github.wanderwise_inc.app.data.SignInLauncher
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.flow.first
@@ -19,66 +22,82 @@ class LoginViewModel(
     private val signInLauncher: SignInLauncher,
     private val isNetworkAvailable: Boolean,
 ) : ViewModel() {
-  // The state of the sign-in process
-  private val _signInState = MutableLiveData(SignInState.NONE)
-  val signInState: LiveData<SignInState>
-    get() = _signInState
+    // The state of the sign-in process
+    private val _signInState = MutableLiveData(SignInState.NONE)
+    val signInState: LiveData<SignInState>
+        get() = _signInState
 
-  /** Launches the sign-in process. */
-  fun signIn() {
-    if (isNetworkAvailable) signInLauncher.signIn()
-    else {
-      Log.d("LoginViewModel", "Network unavailable")
-      _signInState.value = SignInState.OFFLINE
-    }
-  }
-
-  /**
-   * Handles the result of the sign-in process.
-   *
-   * @param profileViewModel The profile view model.
-   * @param user The signed-in user.
-   */
-  suspend fun handleSignInResult(
-      profileViewModel: ProfileViewModel,
-      user: FirebaseUser?,
-  ) {
-    user?.let { signInSucceeded(profileViewModel, it) } ?: signInFailed()
-  }
-
-  /** Sets the sign-in state to [SignInState.FAILURE]. */
-  private fun signInFailed() {
-    _signInState.value = SignInState.FAILURE
-  }
-
-  /**
-   * Sets the sign-in state to [SignInState.SUCCESS] and updates the active profile.
-   *
-   * @param profileViewModel The profile view model.
-   * @param user The signed-in user.
-   */
-  private suspend fun signInSucceeded(profileViewModel: ProfileViewModel, user: FirebaseUser) {
-    // Get the user from database
-    val currentProfile = profileViewModel.getProfile(user.uid).first()
-
-    if (currentProfile != null) {
-      profileViewModel.setActiveProfile(currentProfile)
-    } else {
-      val newProfile = profileViewModel.createProfileFromFirebaseUser(user)
-
-      // Set the user to the database
-      profileViewModel.setProfile(newProfile)
-      profileViewModel.setActiveProfile(newProfile)
+    /** Launches the sign-in process. */
+    fun signIn() {
+        if (isNetworkAvailable) signInLauncher.signIn()
+        else {
+            Log.d("LoginViewModel", "Network unavailable")
+            _signInState.value = SignInState.OFFLINE
+        }
     }
 
-    _signInState.value = SignInState.SUCCESS
-  }
+    /**
+     * Handles the result of the sign-in process.
+     *
+     * @param profileViewModel The profile view model.
+     * @param user The signed-in user.
+     */
+    suspend fun handleSignInResult(
+        profileViewModel: ProfileViewModel,
+        user: FirebaseUser?,
+    ) {
+        user?.let { signInSucceeded(profileViewModel, it) } ?: signInFailed()
+    }
+
+    /** Sets the sign-in state to [SignInState.FAILURE]. */
+    private fun signInFailed() {
+        _signInState.value = SignInState.FAILURE
+    }
+
+    /**
+     * Sets the sign-in state to [SignInState.SUCCESS] and updates the active profile.
+     *
+     * @param profileViewModel The profile view model.
+     * @param user The signed-in user.
+     */
+    private suspend fun signInSucceeded(profileViewModel: ProfileViewModel, user: FirebaseUser) {
+        // Get the user from database
+        val currentProfile = profileViewModel.getProfile(user.uid).first()
+
+        if (currentProfile != null) {
+            profileViewModel.setActiveProfile(currentProfile)
+        } else {
+            val newProfile = profileViewModel.createProfileFromFirebaseUser(user)
+
+            // Set the user to the database
+            profileViewModel.setProfile(newProfile)
+            profileViewModel.setActiveProfile(newProfile)
+        }
+
+        _signInState.value = SignInState.SUCCESS
+    }
+
+    /**
+     * Factory for creating a `LoginViewModel`.
+     */
+    class Factory(
+        private val signInLauncher: SignInLauncher,
+        private val isNetworkAvailable: Boolean,
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return LoginViewModel(signInLauncher, isNetworkAvailable) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
 
 /** The state of the sign-in process. */
 enum class SignInState {
-  NONE,
-  SUCCESS,
-  FAILURE,
-  OFFLINE,
+    NONE,
+    SUCCESS,
+    FAILURE,
+    OFFLINE,
 }
