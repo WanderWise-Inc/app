30502460801b81bac9967bee8a52c9c1043b8afc
Oscar Duong <notzeoscar@gmail.com>
2024-06-02 17:30:29 +0200

    Add app for location notifications

--- app/src/main/java/com/github/wanderwise_inc/app/LocationApp.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/LocationApp.kt
@@ -1,4 +1,21 @@
 package com.github.wanderwise_inc.app
 
-class LocationApp {
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+
+class LocationApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val channel = NotificationChannel(
+            "location",
+            "Location",
+            NotificationManager.IMPORTANCE_LOW
+        )
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
 }
