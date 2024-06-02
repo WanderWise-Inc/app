8aa03ec1557f2f4a0baccd00109395889d85bb03
Oscar Duong <notzeoscar@gmail.com>
2024-06-02 17:36:11 +0200

    Comment

--- app/src/main/java/com/github/wanderwise_inc/app/LocationApp.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/LocationApp.kt
@@ -9,13 +9,18 @@ class LocationApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // Create a new notification channel
         val channel = NotificationChannel(
-            "location",
-            "Location",
-            NotificationManager.IMPORTANCE_LOW
+            "location", // The id of the channel.
+            "Location", // The user-visible name of the channel.
+            NotificationManager.IMPORTANCE_LOW // The importance of the channel. This controls how interruptive notifications posted to this channel are.
         )
 
+        // Get the notification manager
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Create the notification channel
         notificationManager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationService.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationService.kt
@@ -8,6 +8,13 @@ import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import com.github.wanderwise_inc.app.R
 
+/**
+ * `LocationService` is a class that extends the `Service` class.
+ *
+ * This class is used to perform long-running operations in the background.
+ * It is a component that runs in the background to perform long-running operations without providing a user interface.
+ * This manages the location notifications.
+ */
 class LocationService : Service() {
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -23,6 +30,11 @@ class LocationService : Service() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    /**
+     * Starts the service.
+     *
+     * This method creates a notification and starts the service in the foreground.
+     */
     private fun start() {
         val notification = NotificationCompat.Builder(this, "location")
             .setContentTitle("Wanderwise tracking")
@@ -37,10 +49,20 @@ class LocationService : Service() {
         notificationManager.notify(1, notification.build())
     }
 
+    /**
+     * Stops the service.
+     *
+     * This method stops the service by calling `stopSelf()`.
+     */
     private fun stop() {
         stopSelf()
     }
 
+    /**
+     * A companion object is an object that is common to all instances of a class.
+     *
+     * This companion object contains constants that represent the actions that can be performed by the `LocationService`.
+     */
     companion object {
         const val ACTION_START = "ACTION_START"
         const val ACTION_STOP = "ACTION_STOP"
