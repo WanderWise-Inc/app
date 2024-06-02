2e7e850cd428a1ed205016fe4b44227e37503537
Oscar Duong <notzeoscar@gmail.com>
2024-06-02 17:29:57 +0200

    Remove useless functions

--- app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationService.kt
+++ app/src/main/java/com/github/wanderwise_inc/app/viewmodel/LocationService.kt
@@ -1,25 +1,17 @@
 package com.github.wanderwise_inc.app.viewmodel
 
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import com.github.wanderwise_inc.app.R
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
 
 class LocationService : Service() {
 
-    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-
     override fun onBind(intent: Intent?): IBinder? {
         return null
-    }
-
-    override fun onCreate() {
-        super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -34,21 +26,19 @@ class LocationService : Service() {
     private fun start() {
         val notification = NotificationCompat.Builder(this, "location")
             .setContentTitle("Wanderwise tracking")
-            .setContentText("Running")
+            .setContentText("Running...")
             .setSmallIcon(R.mipmap.ic_launcher)
             .setOngoing(true)
 
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         startForeground(1, notification.build())
+
+        notificationManager.notify(1, notification.build())
     }
 
     private fun stop() {
-        stopForeground(STOP_FOREGROUND_REMOVE)
         stopSelf()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        serviceScope.cancel()
     }
 
     companion object {
